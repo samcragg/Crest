7685abf032298e55c027e14f8853b221cb408b78
Samuel Cragg <sam.cragg@outlook.com>
2016-11-03 21:11:14 +0000

    Add marker attributes.

---
+++ src/Crest.Core/AllowAnonymousAttribute.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Core
+{
+    using System;
+
+    /// <summary>
+    /// Marks a method as allowing access from any user (i.e. skip any
+    /// authorization check).
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class AllowAnonymousAttribute : Attribute
+    {
+    }
+}
---
+++ src/Crest.Core/FromBodyAttribute.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Core
+{
+    using System;
+
+    /// <summary>
+    /// Marks a single parameter as receiving the request body.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class FromBodyAttribute : Attribute
+    {
+    }
+}
