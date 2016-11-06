11eb94218ad42f72c34f07321e4f7a33e971f3d3
Samuel Cragg <sam.cragg@outlook.com>
2016-11-06 21:32:18 +0000

    Create substring wrapper

---
+++ src/Crest.Host/StringSegment.cs
@@ -0,0 +1,121 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Contains information about a segment of a string.
+    /// </summary>
+    internal struct StringSegment : IReadOnlyList<char>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringSegment"/> struct.
+        /// </summary>
+        /// <param name="value">The string value.</param>
+        /// <param name="start">The start index of the substring.</param>
+        /// <param name="end">The end index of the substring.</param>
+        public StringSegment(string value, int start, int end)
+        {
+            this.String = value;
+            this.Start = start;
+            this.End = end;
+        }
+
+        /// <summary>
+        /// Gets the number of characters in the substring.
+        /// </summary>
+        public int Count
+        {
+            get { return this.End - this.Start; }
+        }
+
+        /// <summary>
+        /// Gets the index of the character after the substring.
+        /// </summary>
+        public int End { get; }
+
+        /// <summary>
+        /// Gets the index of the first character in the substring.
+        /// </summary>
+        public int Start { get; }
+
+        /// <summary>
+        /// Gets the original string value.
+        /// </summary>
+        public string String { get; }
+
+        /// <summary>
+        /// Gets the character at the specified index relative to the start of
+        /// the substring.
+        /// </summary>
+        /// <param name="index">The index of the character.</param>
+        /// <returns>The character at the specified index.</returns>
+        public char this[int index]
+        {
+            get { return this.String[this.Start + index]; }
+        }
+
+        /// <summary>
+        /// Determines whether the substring pointed to by this instance matches
+        /// the specified string.
+        /// </summary>
+        /// <param name="value">The value to compare to.</param>
+        /// <param name="comparisonType">
+        /// Specifies the rules to use in the comparison.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the substring is equal to the specified value;
+        /// otherwise <c>false</c>.
+        /// </returns>
+        public bool Equals(string value, StringComparison comparisonType)
+        {
+            if ((value != null) && (value.Length > this.Count))
+            {
+                return false;
+            }
+
+            int compare = string.Compare(
+                this.String,
+                this.Start,
+                value,
+                0,
+                this.Count,
+                comparisonType);
+
+            return compare == 0;
+        }
+
+        /// <summary>
+        /// Gets the characters that form the substring.
+        /// </summary>
+        /// <returns>The characters of the sub-section of the string.</returns>
+        public IEnumerator<char> GetEnumerator()
+        {
+            for (int i = this.Start; i < this.End; i++)
+            {
+                yield return this.String[i];
+            }
+        }
+
+        /// <summary>
+        /// Gets the substring represented by this instance.
+        /// </summary>
+        /// <returns>The substring from the original string.</returns>
+        public override string ToString()
+        {
+            return this.String.Substring(this.Start, this.Count);
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ test/Host.UnitTests/StringSegmentTests.cs
@@ -0,0 +1,126 @@
+﻿namespace Host.UnitTests
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using Crest.Host;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class StringSegmentTests
+    {
+        [Test]
+        public void TheConstructorShouldSetTheProperties()
+        {
+            const string ExampleString = "Example";
+
+            var segment = new StringSegment(ExampleString, 1, 5);
+
+            Assert.That(segment.String, Is.EqualTo(ExampleString));
+            Assert.That(segment.Start, Is.EqualTo(1));
+            Assert.That(segment.End, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void CountShouldReturnTheNumberOfCharactersOfTheSubString()
+        {
+            var segment = new StringSegment("string", 2, 3);
+
+            Assert.That(segment.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void IndexShouldReturnTheCharacterRelativeToTheStartOfTheSubString()
+        {
+            var segment = new StringSegment("0123456", 2, 4);
+
+            Assert.That(segment[0], Is.EqualTo('2'));
+            Assert.That(segment[1], Is.EqualTo('3'));
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueIfTheSubStringIsEqual()
+        {
+            var segment = new StringSegment("0123456", 2, 5);
+
+            bool result = segment.Equals("234", StringComparison.Ordinal);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseIfTheSubStringIsNotEqual()
+        {
+            var segment = new StringSegment("0123456", 2, 5);
+
+            bool result = segment.Equals("345", StringComparison.Ordinal);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForNullValues()
+        {
+            var segment = new StringSegment("012", 0, 3);
+
+            bool result = segment.Equals(null, StringComparison.Ordinal);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseIfTheValueIsLongerThanTheSubstring()
+        {
+            var segment = new StringSegment("0123456", 2, 4);
+
+            bool result = segment.Equals("234", StringComparison.Ordinal);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldUseTheComparisonType()
+        {
+            var segment = new StringSegment("abc", 0, 3);
+
+            Assert.That(segment.Equals("AbC", StringComparison.Ordinal), Is.False);
+            Assert.That(segment.Equals("AbC", StringComparison.OrdinalIgnoreCase), Is.True);
+        }
+
+        [Test]
+        public void GetEnumeratorShouldReturnAllOfTheSubString()
+        {
+            var segment = new StringSegment("0123456", 2, 4);
+
+            string subString = new string(Enumerable.ToArray(segment));
+
+            Assert.That(subString, Is.EqualTo("23"));
+        }
+
+        [Test]
+        public void NonGenericGetEnumeratorShouldReturnAllOfTheSubString()
+        {
+            IEnumerable segment = new StringSegment("0123456", 2, 4);
+
+            IEnumerator enumerator = segment.GetEnumerator();
+
+            Assert.That(enumerator.MoveNext(), Is.True);
+            Assert.That(enumerator.Current, Is.EqualTo('2'));
+
+            Assert.That(enumerator.MoveNext(), Is.True);
+            Assert.That(enumerator.Current, Is.EqualTo('3'));
+
+            Assert.That(enumerator.MoveNext(), Is.False);
+        }
+
+        [Test]
+        public void ToStringShouldReturnAllOfTheSubString()
+        {
+            var segment = new StringSegment("0123456", 2, 4);
+
+            string subString = segment.ToString();
+
+            Assert.That(subString, Is.EqualTo("23"));
+        }
+    }
+}
