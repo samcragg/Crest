fcb6eb88352d6dc562a420c097c4639b3caf7d14
Samuel Cragg <sam.cragg@outlook.com>
2016-11-11 22:32:51 +0000

    Parse URL into literals and captures.

---
+++ src/Crest.Host/Routing/UrlParser.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Routing
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Allows the parsing of URLs.
+    /// </summary>
+    internal abstract partial class UrlParser
+    {
+        private enum SegmentType
+        {
+            Literal,
+            Capture,
+            PartialCapture,
+            Error
+        }
+
+        /// <summary>
+        /// Gets the path segments of a URL.
+        /// </summary>
+        /// <param name="url">The URL to parse.</param>
+        /// <returns>A sequence of substrings.</returns>
+        internal static IEnumerable<StringSegment> GetSegments(string url)
+        {
+            int pathEnd = url.IndexOf('?');
+            if (pathEnd < 0)
+            {
+                pathEnd = url.Length;
+            }
+
+            int start = 0;
+            int end = url.IndexOf('/', 0, pathEnd);
+            while (end != -1)
+            {
+                if (start != end)
+                {
+                    yield return new StringSegment(url, start, end);
+                }
+
+                start = end + 1;
+                end = url.IndexOf('/', start, pathEnd - start);
+            }
+
+            if (pathEnd != start)
+            {
+                yield return new StringSegment(url, start, pathEnd);
+            }
+        }
+
+        /// <summary>
+        /// Parses the specified URL, matching the specified parameters.
+        /// </summary>
+        /// <param name="routeUrl">The URL to parse.</param>
+        /// <param name="parameters">The parameter names and types.</param>
+        internal virtual void ParseUrl(string routeUrl, IReadOnlyDictionary<string, Type> parameters)
+        {
+            foreach (StringSegment segment in GetSegments(routeUrl))
+            {
+                string segmentValue;
+                SegmentType type = this.UnescapeSegment(segment, out segmentValue);
+
+                if (type == SegmentType.Capture)
+                {
+                    Type parameterType;
+                    if (!parameters.TryGetValue(segmentValue, out parameterType))
+                    {
+                        this.OnError("Unable to find parameter called: " + segment, segment.Start + 1, segment.Count - 2);
+                        return;
+                    }
+
+                    this.OnCaptureSegment(parameterType, segmentValue);
+                }
+                else if (type == SegmentType.Literal)
+                {
+                    this.OnLiteralSegment(segmentValue);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when a capture segment is parsed.
+        /// </summary>
+        /// <param name="parameterType">The type of the parameter being captured.</param>
+        /// <param name="name">The name of the captured parameter.</param>
+        protected abstract void OnCaptureSegment(Type parameterType, string name);
+
+        /// <summary>
+        /// Called when there is an error parsing the URL.
+        /// </summary>
+        /// <param name="error">The error message.</param>
+        /// <param name="start">The index of where the error starts.</param>
+        /// <param name="length">The length of the string producing the error.</param>
+        protected abstract void OnError(string error, int start, int length);
+
+        /// <summary>
+        /// Called when a literal segment of text is parsed.
+        /// </summary>
+        /// <param name="value">The literal text.</param>
+        protected abstract void OnLiteralSegment(string value);
+
+        private SegmentType UnescapeSegment(StringSegment segment, out string segmentValue)
+        {
+            segmentValue = null;
+
+            var parser = new SegmentParser(this, segment.String, segment.Start, segment.End);
+            if (parser.Type == SegmentType.PartialCapture)
+            {
+                this.OnError("Missing closing brace.", segment.End - 1, 1);
+            }
+            else if (parser.Type != SegmentType.Error)
+            {
+                segmentValue = parser.Value;
+            }
+
+            return parser.Type;
+        }
+    }
+}
---
+++ test/Host.UnitTests/Routing/UrlParserTests.cs
@@ -0,0 +1,124 @@
+﻿namespace Host.UnitTests.Routing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Crest.Host;
+    using Crest.Host.Routing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class UrlParserTests
+    {
+        private FakeUrlParser parser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.parser = new FakeUrlParser();
+        }
+
+        [Test]
+        public void GetSegmentsShouldReturnAllTheParts()
+        {
+            StringSegment[] segments = UrlParser.GetSegments("/one/two").ToArray();
+
+            Assert.That(segments, Has.Length.EqualTo(2));
+            Assert.That(segments[0].ToString(), Is.EqualTo("one"));
+            Assert.That(segments[1].ToString(), Is.EqualTo("two"));
+        }
+
+        [Test]
+        public void ShouldUnescapeBraces()
+        {
+            this.parser.ParseUrl("/{{escaped_braces}}/", new Dictionary<string, Type>());
+
+            Assert.That(this.parser.Literals.Single(), Is.EqualTo("{escaped_braces}"));
+        }
+
+        [Test]
+        public void ShouldCheckForMissingClosingBraces()
+        {
+            this.parser.ParseUrl("/{234/", new Dictionary<string, Type>());
+
+            Assert.That(this.parser.ErrorParts.Single(), Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void ShouldCheckForUnescapedBraces()
+        {
+            this.parser.ParseUrl("/123}/", new Dictionary<string, Type>());
+            this.parser.ParseUrl("/123{/", new Dictionary<string, Type>());
+
+            Assert.That(this.parser.ErrorParts, Is.EqualTo(new[] { "}", "{" }));
+        }
+
+        [Test]
+        public void ShouldCheckForValidParameters()
+        {
+            this.parser.ParseUrl("/{missingParameter}/", new Dictionary<string, Type>());
+
+            Assert.That(this.parser.ErrorParts.Single(), Is.EqualTo("missingParameter"));
+        }
+
+        [Test]
+        public void ShouldCaptureLiterals()
+        {
+            var parser = new FakeUrlParser();
+
+            parser.ParseUrl("/literal/", new Dictionary<string, Type>());
+
+            Assert.That(parser.Literals.Single(), Is.EqualTo("literal"));
+        }
+
+        [Test]
+        public void ShouldCaptureParameters()
+        {
+            var parser = new FakeUrlParser();
+            var parameters = new Dictionary<string, Type>
+            {
+                { "capture", typeof(int) }
+            };
+
+            parser.ParseUrl("/{capture}/", parameters);
+
+            Assert.That(parser.Captures.Single().Item1, Is.EqualTo(typeof(int)));
+            Assert.That(parser.Captures.Single().Item2, Is.EqualTo("capture"));
+        }
+
+        // TODO: Duplicate parameters?
+        // TODO: Unmatched parameters?
+
+        private class FakeUrlParser : UrlParser
+        {
+            private string routeUrl;
+
+            internal List<Tuple<Type, string>> Captures { get; } = new List<Tuple<Type, string>>();
+
+            internal List<string> ErrorParts { get; } = new List<string>();
+
+            internal List<string> Literals { get; } = new List<string>();
+
+            internal override void ParseUrl(string routeUrl, IReadOnlyDictionary<string, Type> parameters)
+            {
+                this.routeUrl = routeUrl;
+                base.ParseUrl(routeUrl, parameters);
+            }
+
+            protected override void OnCaptureSegment(Type parameterType, string name)
+            {
+                this.Captures.Add(Tuple.Create(parameterType, name));
+            }
+
+            protected override void OnError(string error, int start, int length)
+            {
+                this.ErrorParts.Add(this.routeUrl.Substring(start, length));
+            }
+
+            protected override void OnLiteralSegment(string value)
+            {
+                this.Literals.Add(value);
+            }
+        }
+    }
+}
