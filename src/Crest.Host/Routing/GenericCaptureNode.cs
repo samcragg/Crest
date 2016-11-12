6ae6a6bf32696f0797bdd2324f1ed87ecf7aa430
Samuel Cragg <sam.cragg@outlook.com>
2016-11-12 21:40:44 +0000

    Can convert captures from the URL.

---
+++ src/Crest.Host/Routing/GenericCaptureNode.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Routing
+{
+    using System;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Allows the capturing of information from the route and converting it
+    /// with the types default TypeConverter.
+    /// </summary>
+    internal sealed class GenericCaptureNode : IMatchNode
+    {
+        private readonly TypeConverter converter;
+        private readonly string property;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericCaptureNode"/> class.
+        /// </summary>
+        /// <param name="property">
+        /// The name of the property to capture the value to.
+        /// </param>
+        /// <param name="type">
+        /// The type of the property to convert to.
+        /// </param>
+        public GenericCaptureNode(string property, Type type)
+        {
+            this.converter = TypeDescriptor.GetConverter(type);
+            this.property = property;
+        }
+
+        /// <inheritdoc />
+        public int Priority
+        {
+            get { return 200; }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(IMatchNode other)
+        {
+            var node = other as GenericCaptureNode;
+            if (node == null)
+            {
+                return false;
+            }
+
+            return string.Equals(this.property, node.property, StringComparison.Ordinal) &&
+                   (this.converter.GetType() == node.converter.GetType());
+        }
+
+        /// <inheritdoc />
+        public NodeMatchResult Match(StringSegment segment)
+        {
+            if (this.converter.CanConvertFrom(typeof(string)))
+            {
+                // Just because it can convert from a string, doesn't mean the
+                // string is in a valid format, hence Pokemon exception handling...
+                try
+                {
+                    object value = this.converter.ConvertFromInvariantString(segment.ToString());
+                    return new NodeMatchResult(this.property, value);
+                }
+                catch
+                {
+                    // Gotta catch 'em all
+                }
+            }
+
+            return NodeMatchResult.None;
+        }
+    }
+}
---
+++ test/Host.UnitTests/Routing/GenericCaptureNodeTests.cs
@@ -0,0 +1,80 @@
+﻿namespace Host.UnitTests.Routing
+{
+    using System;
+    using Crest.Host;
+    using Crest.Host.Routing;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class GenericCaptureNodeTests
+    {
+        private const string ParameterName = "parameter";
+        private GenericCaptureNode node;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.node = new GenericCaptureNode(ParameterName, typeof(int));
+        }
+
+        [Test]
+        public void PriorityShouldReturnAPositiveValue()
+        {
+            Assert.That(this.node.Priority, Is.Positive);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForNonIntegerCaptureNodes()
+        {
+            IMatchNode other = Substitute.For<IMatchNode>();
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForDifferentParameters()
+        {
+            var other = new GenericCaptureNode(ParameterName + "New", typeof(int));
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForDifferentTypes()
+        {
+            var other = new GenericCaptureNode(ParameterName, typeof(Guid));
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueForTheSameParameter()
+        {
+            var other = new GenericCaptureNode(ParameterName, typeof(int));
+            Assert.That(this.node.Equals(other), Is.True);
+        }
+
+        [Test]
+        public void MatchShouldReturnSuccessIfTheConversionSucceeded()
+        {
+            var result = this.node.Match(new StringSegment("1", 0, 1));
+
+            Assert.That(result.Success, Is.True);
+        }
+
+        [Test]
+        public void MatchShouldReturnTheConvertedParameter()
+        {
+            var result = this.node.Match(new StringSegment("123", 0, 3));
+
+            Assert.That(result.Name, Is.EqualTo(ParameterName));
+            Assert.That(result.Value, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void MatchShouldReturnNoneIfTheConversionErrored()
+        {
+            var result = this.node.Match(new StringSegment("Not an integer", 0, 14));
+
+            Assert.That(result.Success, Is.False);
+        }
+    }
+}
