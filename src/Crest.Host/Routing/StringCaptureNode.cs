451a49b256748221dad85f5204e5f3f3714c9cdc
Samuel Cragg <sam.cragg@outlook.com>
2016-11-12 07:03:55 +0000

    Allow capturing of string parameters.

---
+++ src/Crest.Host/Routing/StringCaptureNode.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Routing
+{
+    using System;
+
+    /// <summary>
+    /// Allows the capturing of string information from the route.
+    /// </summary>
+    internal sealed class StringCaptureNode : IMatchNode
+    {
+        private readonly string property;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringCaptureNode"/> class.
+        /// </summary>
+        /// <param name="property">
+        /// The name of the property to capture the value to.
+        /// </param>
+        public StringCaptureNode(string property)
+        {
+            this.property = property;
+        }
+
+        /// <inheritdoc />
+        public int Priority
+        {
+            get { return 100; }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(IMatchNode other)
+        {
+            var node = other as StringCaptureNode;
+            return string.Equals(this.property, node?.property, StringComparison.Ordinal);
+        }
+
+        /// <inheritdoc />
+        public NodeMatchResult Match(StringSegment segment)
+        {
+            return new NodeMatchResult(this.property, segment.ToString());
+        }
+    }
+}
---
+++ test/Host.UnitTests/Routing/StringCaptureNodeTests.cs
@@ -0,0 +1,66 @@
+﻿namespace Host.UnitTests.Routing
+{
+    using Crest.Host;
+    using Crest.Host.Routing;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class StringCaptureNodeTests
+    {
+        private const string ParameterName = "parameter";
+        private StringCaptureNode node;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.node = new StringCaptureNode(ParameterName);
+        }
+
+        [Test]
+        public void PriorityShouldReturnAPositiveValue()
+        {
+            Assert.That(this.node.Priority, Is.Positive);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForNonStringCaptureNodes()
+        {
+            IMatchNode other = Substitute.For<IMatchNode>();
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForDifferentParameters()
+        {
+            var other = new StringCaptureNode(ParameterName + "New");
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueForTheSameParameter()
+        {
+            var other = new StringCaptureNode(ParameterName);
+            Assert.That(this.node.Equals(other), Is.True);
+        }
+
+        [Test]
+        public void MatchShouldMatchAnyString()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("/string/", 1, 7));
+
+            Assert.That(result.Success, Is.True);
+        }
+
+        [Test]
+        public void MatchShouldSaveTheCapturedParameter()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("01234", 2, 4));
+
+            Assert.That(result.Name, Is.EqualTo(ParameterName));
+            Assert.That(result.Value, Is.EqualTo("23"));
+        }
+    }
+}
