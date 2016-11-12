b8ab1738b4d83013d92a432dac65b1aad2e67106
Samuel Cragg <sam.cragg@outlook.com>
2016-11-12 08:29:59 +0000

    Relax StyleCop rules to allow get/sets without a blank line.

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "StyleCop.CSharp.LayoutRules",
+    "SA1516:Elements must be separated by blank line",
+    Justification = "Rule is not required.")]
