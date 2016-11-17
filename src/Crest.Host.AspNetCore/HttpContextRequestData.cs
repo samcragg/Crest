4a7696afd2ab426cc570d5e2b723050a5970f2cf
Samuel Cragg <sam.cragg@outlook.com>
2016-11-17 06:53:29 +0000

    Create adapter for ASP .NET context.

---
+++ src/Crest.Host.AspNetCore/HttpContextRequestData.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Crest.Host;
+    using Microsoft.AspNetCore.Http;
+
+    /// <summary>
+    /// Allows the conversion from the ASP .NET context to a request data.
+    /// </summary>
+    internal sealed class HttpContextRequestData : IRequestData
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpContextRequestData"/> class.
+        /// </summary>
+        /// <param name="handler">The route handler.</param>
+        /// <param name="parameters">The parameters for the method.</param>
+        /// <param name="context">The current request context.</param>
+        public HttpContextRequestData(MethodInfo handler, IReadOnlyDictionary<string, object> parameters, HttpContext context)
+        {
+            this.Handler = handler;
+            this.Parameters = parameters;
+            this.Url = ConvertToUri(context.Request);
+        }
+
+        /// <inheritdoc />
+        public MethodInfo Handler { get; }
+
+        /// <inheritdoc />
+        public IReadOnlyDictionary<string, object> Parameters { get; }
+
+        /// <inheritdoc />
+        public Uri Url { get; }
+
+        private static Uri ConvertToUri(HttpRequest request)
+        {
+            var builder = new UriBuilder
+            {
+                Host = request.Host.Host,
+                Path = request.Path,
+                Port = request.Host.Port.GetValueOrDefault(-1),
+                Query = request.QueryString.Value,
+                Scheme = request.Scheme
+            };
+
+            return builder.Uri;
+        }
+    }
+}
---
+++ test/Host.AspNetCore.UnitTests/HttpContextRequestDataTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Host.AspNetCore.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Crest.Host.AspNetCore;
+    using Microsoft.AspNetCore.Http;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal sealed class HttpContextRequestDataTests
+    {
+        [Test]
+        public void ShouldAssignTheHandlerProperty()
+        {
+            MethodInfo method = Substitute.For<MethodInfo>();
+
+            var data = new HttpContextRequestData(method, null, CreateContext());
+
+            Assert.That(data.Handler, Is.SameAs(method));
+        }
+
+        [Test]
+        public void ShouldAssignTheParametersProperty()
+        {
+            var parameters = new Dictionary<string, object>();
+
+            var data = new HttpContextRequestData(null, parameters, CreateContext());
+
+            Assert.That(data.Parameters, Is.SameAs(parameters));
+        }
+
+        [Test]
+        public void ShouldCreateTheUrlFromTheContext()
+        {
+            const string Url = "https://host:123/path?query";
+
+            var data = new HttpContextRequestData(null, null, CreateContext(Url));
+
+            Assert.That(data.Url.AbsoluteUri, Is.EqualTo(Url));
+        }
+
+        private static HttpContext CreateContext(string urlString = "http://localhost")
+        {
+            Uri url = new Uri(urlString);
+            HttpContext context = Substitute.For<HttpContext>();
+            context.Request.Host = new HostString(url.Host, url.Port);
+            context.Request.Path = new PathString(url.AbsolutePath);
+            context.Request.QueryString = new QueryString(url.Query);
+            context.Request.Scheme = url.Scheme;
+            return context;
+        }
+    }
+}
