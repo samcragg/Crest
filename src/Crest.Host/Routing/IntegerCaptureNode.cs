feb939f8640cd995a2d05f4b99d8a872c6514647
Samuel Cragg <sam.cragg@outlook.com>
2016-12-06 06:47:07 +0000

    Capture integers from the URL

---
+++ src/Crest.Host/Routing/IntegerCaptureNode.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Routing
+{
+    using System;
+    using Crest.Host.Conversion;
+
+    /// <summary>
+    /// Allows the capturing of integer values from the route.
+    /// </summary>
+    internal sealed class IntegerCaptureNode : IMatchNode
+    {
+        private readonly string property;
+        private readonly IntegerType type;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntegerCaptureNode"/> class.
+        /// </summary>
+        /// <param name="property">
+        /// The name of the property to capture the value to.
+        /// </param>
+        /// <param name="targetType">The type of the integer.</param>
+        public IntegerCaptureNode(string property, Type targetType)
+        {
+            this.property = property;
+            this.type = GetIntegerType(targetType);
+        }
+
+        private enum IntegerType
+        {
+            Byte,
+            Int16,
+            Int32,
+            Int64,
+            SByte,
+            UInt16,
+            UInt32,
+            UInt64
+        }
+
+        /// <inheritdoc />
+        public int Priority
+        {
+            get { return 500; }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(IMatchNode other)
+        {
+            var node = other as IntegerCaptureNode;
+            if (node == null)
+            {
+                return false;
+            }
+
+            return (this.type == node.type) &&
+                    string.Equals(this.property, node.property, StringComparison.Ordinal);
+        }
+
+        /// <inheritdoc />
+        public NodeMatchResult Match(StringSegment segment)
+        {
+            long value;
+            if (IntegerConverter.ParseSignedValue(segment, out value))
+            {
+                return new NodeMatchResult(this.property, this.BoxInteger(value));
+            }
+            else
+            {
+                return NodeMatchResult.None;
+            }
+        }
+
+        private static IntegerType GetIntegerType(Type type)
+        {
+            if (string.Equals("System", type.Namespace, StringComparison.Ordinal))
+            {
+                IntegerType integerType;
+                if (Enum.TryParse(type.Name, out integerType))
+                {
+                    return integerType;
+                }
+            }
+
+            throw new ArgumentException("Unknown integer type {0}", type.FullName);
+        }
+
+        private object BoxInteger(long value)
+        {
+            // We need to box it as the correct type as unboxing will not do the
+            // conversion for us (i.e. object b = 1; short goesBang = (short)b;)
+            switch (this.type)
+            {
+                case IntegerType.Byte:
+                    return (byte)value;
+
+                case IntegerType.Int16:
+                    return (short)value;
+
+                case IntegerType.Int32:
+                    return (int)value;
+
+                case IntegerType.Int64:
+                    return value;
+
+                case IntegerType.SByte:
+                    return (sbyte)value;
+
+                case IntegerType.UInt16:
+                    return (ushort)value;
+
+                case IntegerType.UInt32:
+                    return (uint)value;
+
+                case IntegerType.UInt64:
+                    return (ulong)value;
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Routing/IntegerCaptureNodeTests.cs
@@ -0,0 +1,89 @@
+﻿namespace Host.UnitTests.Routing
+{
+    using System;
+    using Crest.Host;
+    using Crest.Host.Routing;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class IntegerCaptureNodeTests
+    {
+        private const string ParameterName = "parameter";
+        private IntegerCaptureNode node;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.node = new IntegerCaptureNode(ParameterName, typeof(int));
+        }
+
+        [Test]
+        public void ConstructorShouldThrowForInvalidIntegerTypes()
+        {
+            Assert.That(
+                () => new IntegerCaptureNode("", typeof(Guid)),
+                Throws.InstanceOf<ArgumentException>());
+
+            Assert.That(
+                () => new IntegerCaptureNode("", typeof(IntegerCaptureNodeTests)),
+                Throws.InstanceOf<ArgumentException>());
+        }
+
+        [Test]
+        public void PriorityShouldReturnAPositiveValue()
+        {
+            Assert.That(this.node.Priority, Is.Positive);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForNonIntegerCaptureNodes()
+        {
+            IMatchNode other = Substitute.For<IMatchNode>();
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForDifferentParameters()
+        {
+            var other = new IntegerCaptureNode(ParameterName + "New", typeof(int));
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForDifferentTypes()
+        {
+            var other = new IntegerCaptureNode(ParameterName, typeof(short));
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueForTheSameParameter()
+        {
+            var other = new IntegerCaptureNode(ParameterName, typeof(int));
+            Assert.That(this.node.Equals(other), Is.True);
+        }
+
+        [TestCase("123", ExpectedResult = 123)]
+        [TestCase("+123", ExpectedResult = 123)]
+        [TestCase("-123", ExpectedResult = -123)]
+        public int ShouldMatchValidIntegers(string integer)
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("/" + integer + "/", 1, integer.Length + 1));
+
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Name, Is.EqualTo(ParameterName));
+            return (int)result.Value;
+        }
+
+        [Test]
+        public void ShouldNotMatchInvalidIntegers()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("/ABC/", 1, 4));
+
+            Assert.That(result.Success, Is.False);
+        }
+    }
+}
