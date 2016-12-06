70f52771360c72318883f226a463fcb4826f6d19
Samuel Cragg <sam.cragg@outlook.com>
2016-12-06 06:32:02 +0000

    Capture booleans from the URL

---
+++ src/Crest.Host/Routing/BoolCaptureNode.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Routing
+{
+    using System;
+
+    /// <summary>
+    /// Allows the capturing of boolean values from the route.
+    /// </summary>
+    internal sealed class BoolCaptureNode : IMatchNode
+    {
+        private static readonly object BoxedFalse = false;
+        private static readonly object BoxedTrue = true;
+        private static readonly string[] FalseValues = { "false", "0" };
+        private static readonly string[] TrueValues = { "true", "1" };
+        private readonly string property;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoolCaptureNode"/> class.
+        /// </summary>
+        /// <param name="property">
+        /// The name of the property to capture the value to.
+        /// </param>
+        public BoolCaptureNode(string property)
+        {
+            this.property = property;
+        }
+
+        /// <inheritdoc />
+        public int Priority
+        {
+            get { return 500; }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(IMatchNode other)
+        {
+            var node = other as BoolCaptureNode;
+            return string.Equals(this.property, node?.property, StringComparison.Ordinal);
+        }
+
+        /// <inheritdoc />
+        public NodeMatchResult Match(StringSegment segment)
+        {
+            if (Matches(segment, FalseValues))
+            {
+                return new NodeMatchResult(this.property, BoxedFalse);
+            }
+            else if (Matches(segment, TrueValues))
+            {
+                return new NodeMatchResult(this.property, BoxedTrue);
+            }
+            else
+            {
+                return NodeMatchResult.None;
+            }
+        }
+
+        private static bool Matches(StringSegment segment, string[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (segment.Equals(values[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/Host.UnitTests/Routing/BoolCaptureNodeTests.cs
@@ -0,0 +1,103 @@
+﻿namespace Host.UnitTests.Routing
+{
+    using Crest.Host;
+    using Crest.Host.Routing;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class BoolCaptureNodeTests
+    {
+        private const string ParameterName = "parameter";
+        private BoolCaptureNode node;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.node = new BoolCaptureNode(ParameterName);
+        }
+
+        [Test]
+        public void PriorityShouldReturnAPositiveValue()
+        {
+            Assert.That(this.node.Priority, Is.Positive);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForNonBoolCaptureNodes()
+        {
+            IMatchNode other = Substitute.For<IMatchNode>();
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseForDifferentParameters()
+        {
+            var other = new BoolCaptureNode(ParameterName + "New");
+            Assert.That(this.node.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueForTheSameParameter()
+        {
+            var other = new BoolCaptureNode(ParameterName);
+            Assert.That(this.node.Equals(other), Is.True);
+        }
+
+        [Test]
+        public void MatchShouldMatchFalse()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("/False/", 1, 6));
+
+            Assert.That(result.Success, Is.True);
+        }
+
+        [Test]
+        public void MatchShouldMatchTrue()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("/True/", 1, 5));
+
+            Assert.That(result.Success, Is.True);
+        }
+
+        [Test]
+        public void MatchShouldNotMatchPartialWords()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("_true_", 1, 4));
+
+            Assert.That(result.Success, Is.False);
+        }
+
+        [Test]
+        public void MatchShouldReturnTheCapturedParameter()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("true", 0, 4));
+
+            Assert.That(result.Name, Is.EqualTo(ParameterName));
+        }
+
+        [Test]
+        public void MatchShouldMatchZeroAsFalse()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("_0_", 1, 2));
+
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Value, Is.False);
+        }
+
+        [Test]
+        public void MatchShouldMatchOneAsTrue()
+        {
+            NodeMatchResult result = this.node.Match(
+                new StringSegment("_1_", 1, 2));
+
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Value, Is.True);
+        }
+    }
+}
