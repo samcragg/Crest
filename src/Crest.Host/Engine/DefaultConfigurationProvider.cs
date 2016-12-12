902a1c89365fb46dd2a402c3f7af2a3c4861d39a
Samuel Cragg <sam.cragg@outlook.com>
2016-12-12 07:09:21 +0000

    Add default value injector for configuration classes.

---
+++ src/Crest.Host/Engine/DefaultConfigurationProvider.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Provides the default value for the properties in a configuration class.
+    /// </summary>
+    internal sealed class DefaultConfigurationProvider : IConfigurationProvider
+    {
+        private readonly Dictionary<Type, Action<object>> initialize =
+            new Dictionary<Type, Action<object>>();
+
+        /// <inheritdoc />
+        public int Order
+        {
+            get { return 100; }
+        }
+
+        /// <inheritdoc />
+        public Task Initialize(IEnumerable<Type> knownTypes)
+        {
+            return Task.Run(() =>
+            {
+                foreach (Type type in knownTypes)
+                {
+                    Action<object> initialize = CreateInitializeDelegate(type);
+                    if (initialize != null)
+                    {
+                        this.initialize.Add(type, initialize);
+                    }
+                }
+            });
+        }
+
+        /// <inheritdoc />
+        public void Inject(object instance)
+        {
+            Action<object> initialize;
+            if (this.initialize.TryGetValue(instance.GetType(), out initialize))
+            {
+                initialize(instance);
+            }
+        }
+
+        private static Action<object> CreateInitializeDelegate(Type type)
+        {
+            ParameterExpression instance = Expression.Parameter(type);
+            Expression setProperties = SetDefaults(type, instance);
+            if (setProperties == null)
+            {
+                return null;
+            }
+
+            var parameter = Expression.Parameter(typeof(object));
+            Expression body = Expression.Block(
+                new[] { instance },
+                Expression.Assign(instance, Expression.Convert(parameter, type)),
+                setProperties);
+
+            return Expression.Lambda<Action<object>>(body, parameter).Compile();
+        }
+
+        private static Expression SetDefaults(Type type, Expression instance)
+        {
+            PropertyInfo[] properties = type.GetProperties();
+            if (properties.Length > 0)
+            {
+                var body = new List<Expression>();
+                var locals = new List<ParameterExpression>();
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    SetProperty(instance, body, locals, properties[i]);
+                }
+
+                // Only return an expression if there are any properties to set
+                if (body.Count > 0)
+                {
+                    return Expression.Block(locals, body);
+                }
+            }
+
+            return null;
+        }
+
+        private static void SetProperty(Expression instance, IList<Expression> body, IList<ParameterExpression> locals, PropertyInfo property)
+        {
+            DefaultValueAttribute defaultValue = property.GetCustomAttribute<DefaultValueAttribute>();
+            if (defaultValue != null)
+            {
+                // Simple case - just set the property to the default value
+                body.Add(Expression.Assign(
+                    Expression.Property(instance, property),
+                    Expression.Constant(defaultValue.Value)));
+            }
+            else if (!property.PropertyType.GetTypeInfo().IsValueType)
+            {
+                // We need to see if any of the nested properties need setting.
+                // If they do we'll need to do a null check, so use a local
+                // variable to store the value, however, since we don't know at
+                // this stage whether any nested properties need setting don't
+                // add the null check+local until we have something to set.
+                ParameterExpression local = Expression.Parameter(property.PropertyType);
+                Expression setNestedProperties = SetDefaults(property.PropertyType, local);
+                if (setNestedProperties != null)
+                {
+                    // var local = instance.Property;
+                    // if (local != null) { SetDefaults(local); }
+                    locals.Add(local);
+                    body.Add(Expression.Assign(local, Expression.Property(instance, property)));
+                    body.Add(Expression.IfThen(
+                        Expression.IsFalse(Expression.Equal(local, Expression.Constant(null))),
+                        setNestedProperties));
+                }
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Engine/DefaultConfigurationProviderTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Host.UnitTests.Engine
+{
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using Crest.Host.Engine;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class DefaultConfigurationProviderTests
+    {
+        private DefaultConfigurationProvider provider;
+        [SetUp]
+        public void SetUp()
+        {
+            this.provider = new DefaultConfigurationProvider();
+        }
+
+        [Test]
+        public void OrderShouldReturnAPositiveValue()
+        {
+            Assert.That(provider.Order, Is.Positive);
+        }
+
+        [Test]
+        public async Task InjectShouldSetTheDefaultValuesOfProperties()
+        {
+            await this.provider.Initialize(new[] { typeof(HasDefaultProperties) });
+            var instance = new HasDefaultProperties();
+
+            this.provider.Inject(instance);
+
+            Assert.That(instance.Integer, Is.EqualTo(HasDefaultProperties.IntegerDefault));
+        }
+
+        [Test]
+        public async Task InjectShouldHandleTypesWithNoDefaultProperties()
+        {
+            await this.provider.Initialize(new[] { typeof(NoDefaultProperties) });
+            var instance = new NoDefaultProperties();
+
+            this.provider.Inject(instance);
+
+            Assert.That(instance.String, Is.Null);
+        }
+
+        [Test]
+        public async Task InjectShouldSetNestedProperties()
+        {
+            await this.provider.Initialize(new[] { typeof(HasNestedProperties) });
+            var instance = new HasNestedProperties();
+
+            this.provider.Inject(instance);
+
+            Assert.That(instance.NotNullNested.Integer, Is.EqualTo(HasDefaultProperties.IntegerDefault));
+        }
+
+        private class HasDefaultProperties
+        {
+            internal const int IntegerDefault = 123;
+
+            [DefaultValue(IntegerDefault)]
+            public int Integer { get; set; }
+        }
+
+        private class NoDefaultProperties
+        {
+            public string String { get; set; }
+        }
+
+        private class HasNestedProperties
+        {
+            public HasDefaultProperties NotNullNested { get; } = new HasDefaultProperties();
+
+            public HasDefaultProperties NullProperty { get; }
+        }
+    }
+}
