e3e5b673b503992f06a4529e2b0a4033df2638c1
Samuel Cragg <sam.cragg@outlook.com>
2016-12-31 06:56:36 +0000

    Generate infoObject for OpenAPI.

---
+++ tools/Crest.OpenApi/InfoObjectWriter.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.OpenApi
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Reflection;
+
+    /// <summary>
+    /// Allows the writing of the OpenAPI information.
+    /// </summary>
+    /// <remarks>
+    /// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#infoObject
+    /// </remarks>
+    internal sealed class InfoObjectWriter : JsonWriter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InfoObjectWriter"/> class.
+        /// </summary>
+        /// <param name="writer">Where to write the output to.</param>
+        public InfoObjectWriter(TextWriter writer)
+            : base(writer)
+        {
+        }
+
+        /// <summary>
+        /// Writes the InfoObject for the specified version.
+        /// </summary>
+        /// <param name="version">The version number.</param>
+        /// <param name="assembly">The assembly information.</param>
+        public void WriteInformation(int version, Assembly assembly)
+        {
+            var converter = new AttributeConverter();
+            foreach (Attribute attribute in assembly.GetCustomAttributes())
+            {
+                converter.ParseAttribute(attribute);
+            }
+
+            this.WriteTitle(assembly, converter);
+            this.WriteDescription(converter);
+            this.WriteLicense(converter);
+            this.WriteVersion(version);
+        }
+
+        private void WriteDescription(AttributeConverter converter)
+        {
+            if (!string.IsNullOrWhiteSpace(converter.Description))
+            {
+                this.WriteRaw(",\"description\":");
+                this.WriteString(converter.Description);
+            }
+        }
+
+        private void WriteLicense(AttributeConverter converter)
+        {
+            if (string.IsNullOrWhiteSpace(converter.License))
+            {
+                if (!string.IsNullOrWhiteSpace(converter.LicenseUrl))
+                {
+                    Console.WriteLine("LicenseUrl is being ignored as no license is specified.");
+                }
+            }
+            else
+            {
+                this.WriteRaw(",\"license\":{\"name\":");
+                this.WriteString(converter.License);
+
+                if (!string.IsNullOrWhiteSpace(converter.LicenseUrl))
+                {
+                    this.WriteRaw(",\"url\":");
+                    this.WriteString(converter.LicenseUrl);
+                }
+
+                this.Write('}');
+            }
+        }
+
+        private void WriteTitle(Assembly assembly, AttributeConverter converter)
+        {
+            this.WriteRaw("\"title\":");
+
+            if (string.IsNullOrWhiteSpace(converter.Title))
+            {
+                this.WriteString(assembly.GetName().Name);
+            }
+            else
+            {
+                this.WriteString(converter.Title);
+            }
+        }
+
+        private void WriteVersion(int version)
+        {
+            this.WriteRaw(",\"version\":");
+            this.WriteString(version.ToString(NumberFormatInfo.InvariantInfo));
+        }
+
+        private class AttributeConverter
+        {
+            public string Description { get; private set; }
+
+            public string License { get; private set; }
+
+            public string LicenseUrl { get; private set; }
+
+            public string Title { get; private set; }
+
+            public void ParseAttribute(Attribute attribute)
+            {
+                var title = attribute as AssemblyTitleAttribute;
+                if (title != null)
+                {
+                    this.Title = title.Title;
+                }
+                else
+                {
+                    var description = attribute as AssemblyDescriptionAttribute;
+                    if (description != null)
+                    {
+                        this.Description = description.Description;
+                    }
+                    else
+                    {
+                        var metadata = attribute as AssemblyMetadataAttribute;
+                        if (metadata != null)
+                        {
+                            this.ParseMetadata(metadata.Key, metadata.Value);
+                        }
+                    }
+                }
+            }
+
+            private void ParseMetadata(string key, string value)
+            {
+                if (string.Equals(key, "License", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.License = value;
+                }
+                else if (string.Equals(key, "LicenseUrl", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.LicenseUrl = value;
+                }
+            }
+        }
+    }
+}
---
+++ tools/OpenApi.UnitTests/InfoObjectWriterTests.cs
@@ -0,0 +1,113 @@
+﻿namespace OpenApi.UnitTests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using Crest.OpenApi;
+    using Newtonsoft.Json;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class InfoObjectWriterTests
+    {
+        private const string GeneratedAssemblyName = "AssemblyName";
+
+        [Test]
+        public void ShouldOutputTheAssemblyNameIfThereIsNoTitleAttribute()
+        {
+            dynamic result = this.GetInformation(1, CreateAssembly());
+
+            Assert.That((string)result.title, Is.EqualTo(GeneratedAssemblyName));
+        }
+
+        [Test]
+        public void ShouldOutputTheVersion()
+        {
+            dynamic result = this.GetInformation(2, CreateAssembly());
+
+            Assert.That((string)result.version, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void ShouldOutputTheAssemblyDescriptionAttribute()
+        {
+            var assembly = CreateAssembly(() => new AssemblyDescriptionAttribute("Assembly description"));
+
+            dynamic result = this.GetInformation(1, assembly);
+
+            Assert.That((string)result.description, Is.EqualTo("Assembly description"));
+        }
+
+        [Test]
+        public void ShouldOutputTheAssemblyTitleAttribute()
+        {
+            var assembly = CreateAssembly(() => new AssemblyTitleAttribute("Assembly Title"));
+
+            dynamic result = this.GetInformation(1, assembly);
+
+            Assert.That((string)result.title, Is.EqualTo("Assembly Title"));
+        }
+
+        [Test]
+        public void ShouldOutputTheLicenseName()
+        {
+            var assembly = CreateAssembly(() => new AssemblyMetadataAttribute("License", "License name"));
+
+            dynamic result = this.GetInformation(1, assembly);
+
+            Assert.That((string)result.license.name, Is.EqualTo("License name"));
+        }
+
+        [Test]
+        public void ShouldOutputTheLicenseUrl()
+        {
+            var assembly = CreateAssembly(
+                () => new AssemblyMetadataAttribute("License", "License name"),
+                () => new AssemblyMetadataAttribute("LicenseUrl", "http://www.example.com"));
+
+            dynamic result = this.GetInformation(1, assembly);
+
+            Assert.That((string)result.license.url, Is.EqualTo("http://www.example.com"));
+        }
+
+        [Test]
+        public void ShouldNotOutputTheLicenseUrlIfTheLicenseNameIsMissing()
+        {
+            var assembly = CreateAssembly(() => new AssemblyMetadataAttribute("LicenseUrl", "http://www.example.com"));
+
+            dynamic result = this.GetInformation(1, assembly);
+
+            Assert.That(result.license, Is.Null);
+        }
+
+        private Assembly CreateAssembly(params Expression<Func<Attribute>>[] attributes)
+        {
+            AssemblyBuilder builder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(GeneratedAssemblyName), AssemblyBuilderAccess.Run);
+            foreach (Expression<Func<Attribute>> attribute in attributes)
+            {
+                var constructorCall = attribute.Body as NewExpression;
+                ConstructorInfo constructor = constructorCall.Constructor;
+                object[] arguments = constructorCall.Arguments
+                                                    .Select(a => ((ConstantExpression)a).Value)
+                                                    .ToArray();
+
+                builder.SetCustomAttribute(new CustomAttributeBuilder(constructor, arguments));
+            }
+
+            return builder;
+        }
+
+        private dynamic GetInformation(int version, Assembly assembly)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                var infoObjectWriter = new InfoObjectWriter(stringWriter);
+                infoObjectWriter.WriteInformation(version, assembly);
+                return JsonConvert.DeserializeObject("{" + stringWriter.ToString() + "}");
+            }
+        }
+    }
+}
