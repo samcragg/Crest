de10a905faf85051d0fa44856cde1b8cb95bfdf5
Samuel Cragg <sam.cragg@outlook.com>
2017-01-01 08:20:16 +0000

    Allow the writing of tags for OpenAPI paths.

---
+++ tools/Crest.OpenApi/TagWriter.cs
@@ -0,0 +1,122 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.OpenApi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// Allows the writing of tags.
+    /// </summary>
+    /// <remarks>
+    /// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#tagObject
+    /// </remarks>
+    internal sealed class TagWriter : JsonWriter
+    {
+        // The specification says "The order of the tags can be used to reflect
+        // on their order by the parsing tools" so we'll keep the insertion order
+        // hence a list and not dictionary.
+        private readonly List<TagItem> tags = new List<TagItem>();
+        private readonly XmlDocParser xmlDoc;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TagWriter"/> class.
+        /// </summary>
+        /// <param name="xmlDoc">Contains the parsed XML documentation.</param>
+        /// <param name="writer">Where to write the output to.</param>
+        public TagWriter(XmlDocParser xmlDoc, TextWriter writer)
+            : base(writer)
+        {
+            this.xmlDoc = xmlDoc;
+        }
+
+        /// <summary>
+        /// Creates a tag for the specified interface type.
+        /// </summary>
+        /// <param name="interfaceType">The type of the interface.</param>
+        /// <returns>The tag name.</returns>
+        public string CreateTag(Type interfaceType)
+        {
+            // Spec says "Each tag name in the list MUST be unique"
+            string name = GetTagName(interfaceType);
+            if (!this.tags.Any(i => string.Equals(i.Name, name, StringComparison.Ordinal)))
+            {
+                ClassDescription description = this.xmlDoc.GetClassDescription(interfaceType);
+                this.tags.Add(new TagItem
+                {
+                    Name = name,
+                    Description = description?.Summary
+                });
+            }
+
+            return name;
+        }
+
+        /// <summary>
+        /// Writes all the created tags to the output.
+        /// </summary>
+        public void WriteTags()
+        {
+            this.Write('[');
+
+            for (int i = 0; i < this.tags.Count; i++)
+            {
+                if (i != 0)
+                {
+                    this.Write(',');
+                }
+
+                this.WriteRaw("{\"name\":");
+                this.WriteString(this.tags[i].Name);
+                this.WriteDescription(this.tags[i].Description);
+                this.Write('}');
+            }
+
+            this.Write(']');
+        }
+
+        private static string GetTagName(Type type)
+        {
+            var description = type.GetTypeInfo().GetCustomAttribute<DescriptionAttribute>();
+            if (description != null)
+            {
+                return description.Description;
+            }
+
+            string name = type.Name;
+            if (name[0] == 'I')
+            {
+                return name.Substring(1);
+            }
+            else
+            {
+                return name;
+            }
+        }
+
+        private void WriteDescription(string description)
+        {
+            if (!string.IsNullOrEmpty(description))
+            {
+                description = description.Trim();
+                description = description.TrimEnd('.');
+                this.WriteRaw(",\"description\":");
+                this.WriteString(description);
+            }
+        }
+
+        private struct TagItem
+        {
+            public string Description { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ tools/OpenApi.UnitTests/TagWriterTests.cs
@@ -0,0 +1,82 @@
+﻿namespace OpenApi.UnitTests
+{
+    using System.IO;
+    using Crest.OpenApi;
+    using Newtonsoft.Json;
+    using NSubstitute;
+    using NUnit.Framework;
+    using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+
+    [TestFixture]
+    public sealed class TagWriterTests
+    {
+        private XmlDocParser xmlDoc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.xmlDoc = Substitute.For<XmlDocParser>();
+        }
+
+        [Test]
+        public void CreateTagShouldUseTheDescriptionAttributeForTheTagName()
+        {
+            var writer = new TagWriter(this.xmlDoc, null);
+
+            string result = writer.CreateTag(typeof(IWithDescription));
+
+            Assert.That(result, Is.EqualTo("DescriptionText"));
+        }
+
+        [Test]
+        public void CreateTagShouldUseTheeInterfaceName()
+        {
+            var writer = new TagWriter(this.xmlDoc, null);
+
+            string result = writer.CreateTag(typeof(IFakeInterface));
+
+            Assert.That(result, Is.EqualTo("FakeInterface"));
+        }
+
+        [Test]
+        public void WriteTagsShouldOutputTheTagName()
+        {
+            dynamic result = this.GetOutput<IFakeInterface>();
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That((string)result[0].name, Is.EqualTo("FakeInterface"));
+        }
+
+        [Test]
+        public void WriteTagsShouldOutputTheInterfaceSummaryWithoutTrailingFullstop()
+        {
+            this.xmlDoc.GetClassDescription(typeof(IFakeInterface))
+                .Returns(new ClassDescription { Summary = "Summary information." });
+
+            dynamic result = this.GetOutput<IFakeInterface>();
+
+            Assert.That((string)result[0].description, Is.EqualTo("Summary information"));
+        }
+
+        private dynamic GetOutput<T>()
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                var tagWriter = new TagWriter(this.xmlDoc, stringWriter);
+                tagWriter.CreateTag(typeof(T));
+                tagWriter.WriteTags();
+
+                return JsonConvert.DeserializeObject(stringWriter.ToString());
+            }
+        }
+
+        private interface IFakeInterface
+        {
+        }
+
+        [Description("DescriptionText")]
+        private interface IWithDescription
+        {
+        }
+    }
+}
