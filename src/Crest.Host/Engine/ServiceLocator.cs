1e34b5165559874d98360f65ced18310d07f7ad9
Samuel Cragg <sam.cragg@outlook.com>
2017-01-19 07:06:21 +0000

    Allow built in types to be switched out.

---
+++ src/Crest.Host/Engine/ServiceLocator.cs
@@ -0,0 +1,216 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using Crest.Host.Conversion;
+    using DryIoc;
+
+    /// <summary>
+    /// Creates the instances of interfaces required during initialization.
+    /// </summary>
+    public class ServiceLocator : IDisposable
+    {
+        private readonly IContainer container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceLocator"/> class.
+        /// </summary>
+        public ServiceLocator()
+            : this(new Container())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceLocator"/> class.
+        /// </summary>
+        /// <param name="container">The container to use.</param>
+        /// <remarks>This constructor is to allow unit testing.</remarks>
+        internal ServiceLocator(IContainer container)
+        {
+            this.container = container;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="ServiceLocator"/> class.
+        /// </summary>
+        ~ServiceLocator()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the object has been disposed or not.
+        /// </summary>
+        protected bool IsDisposed
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Releases all resources used by this instance.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Gets the registered plugins to call after processing a request.
+        /// </summary>
+        /// <returns>A sequence of registered plugins.</returns>
+        public virtual IPostRequestPlugin[] GetAfterRequestPlugins()
+        {
+            this.ThrowIfDisposed();
+
+            return this.Resolve<IPostRequestPlugin[]>();
+        }
+
+        /// <summary>
+        /// Gets the registered plugins to call before processing a request.
+        /// </summary>
+        /// <returns>A sequence of registered plugins.</returns>
+        public virtual IPreRequestPlugin[] GetBeforeRequestPlugins()
+        {
+            this.ThrowIfDisposed();
+
+            return this.Resolve<IPreRequestPlugin[]>();
+        }
+
+        /// <summary>
+        /// Gets the service to use for providing configuration data.
+        /// </summary>
+        /// <returns>An object implementing <see cref="IDiscoveryService"/>.</returns>
+        public virtual ConfigurationService GetConfigurationService()
+        {
+            this.ThrowIfDisposed();
+
+            var providers = this.Resolve<IEnumerable<IConfigurationProvider>>();
+            return new ConfigurationService(providers);
+        }
+
+        /// <summary>
+        /// Gets the service to use for discovering assemblies and types.
+        /// </summary>
+        /// <returns>An object implementing <see cref="IDiscoveryService"/>.</returns>
+        public virtual IDiscoveryService GetDiscoveryService()
+        {
+            this.ThrowIfDisposed();
+
+            return this.TryResolve<IDiscoveryService, DiscoveryService>();
+        }
+
+        /// <summary>
+        /// Gets the registered plugins to handle generated exceptions.
+        /// </summary>
+        /// <returns>A sequence of registered plugins.</returns>
+        public virtual IErrorHandlerPlugin[] GetErrorHandlers()
+        {
+            this.ThrowIfDisposed();
+
+            return this.Resolve<IErrorHandlerPlugin[]>();
+        }
+
+        /// <summary>
+        /// Gets the service to use for providing the template for generated HTML.
+        /// </summary>
+        /// <returns>An object implementing <see cref="IHtmlTemplateProvider"/>.</returns>
+        public virtual IHtmlTemplateProvider GetHtmlTemplateProvider()
+        {
+            this.ThrowIfDisposed();
+
+            return this.TryResolve<IHtmlTemplateProvider, HtmlTemplateProvider>();
+        }
+
+        /// <summary>
+        /// Returns an instance of the specified concrete type whilst ensuring
+        /// any dependencies are injected.
+        /// </summary>
+        /// <typeparam name="T">The type to create.</typeparam>
+        /// <returns>An instance of the specified class.</returns>
+        protected virtual T Create<T>()
+            where T : class
+        {
+            return this.container.New<T>();
+        }
+
+        /// <summary>
+        /// Releases the unmanaged resources used by this instance and
+        /// optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <c>true</c> to release both managed and unmanaged resources;
+        /// <c>false</c> to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.IsDisposed)
+            {
+                if (disposing)
+                {
+                    this.container.Dispose();
+                }
+
+                this.IsDisposed = true;
+            }
+        }
+
+        //// /// <summary>
+        //// /// Registers a service as being implemented by the specified type.
+        //// /// </summary>
+        //// /// <typeparam name="TService">The service type.</typeparam>
+        //// /// <typeparam name="TImplementation">
+        //// /// The type that implements the service.
+        //// /// </typeparam>
+        //// protected virtual void RegisterTransient<TService, TImplementation>()
+        ////     where TImplementation : TService
+        //// {
+        ////     this.container.Register<TService, TImplementation>();
+        //// }
+
+        /// <summary>
+        /// Returns an instance of the specified service.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <returns>An instance of the specified service.</returns>
+        protected virtual TService Resolve<TService>()
+        {
+            return this.container.Resolve<TService>();
+        }
+
+        /// <summary>
+        /// Raises the <see cref="ObjectDisposedException"/> if <see cref="Dispose()"/>
+        /// has been called on this instance.
+        /// </summary>
+        protected void ThrowIfDisposed()
+        {
+            if (this.IsDisposed)
+            {
+                throw new ObjectDisposedException(this.GetType().Name);
+            }
+        }
+
+        private TService TryResolve<TService, TFallback>()
+            where TFallback : class, TService
+        {
+            TService[] services = this.Resolve<TService[]>();
+            switch (services.Length)
+            {
+                case 0:
+                    return this.Create<TFallback>();
+
+                case 1:
+                    return services[0];
+
+                default:
+                    throw new InvalidOperationException("Multiple services were found for " + typeof(TService).Name);
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Engine/ServiceLocatorTests.cs
@@ -0,0 +1,236 @@
+﻿namespace Host.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Crest.Host;
+    using Crest.Host.Engine;
+    using DryIoc;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class ServiceLocatorTests
+    {
+        private IContainer container;
+        private FakeServiceLocator locator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.container = Substitute.For<IContainer>();
+            this.locator = new FakeServiceLocator(this.container);
+        }
+
+        [Test]
+        public void DiposeShouldSetIsDisposedToTrue()
+        {
+            Assert.That(this.locator.IsDisposed, Is.False);
+
+            this.locator.Dispose();
+
+            Assert.That(this.locator.IsDisposed, Is.True);
+        }
+
+        [Test]
+        public void DisposeCanBeCalledMultipleTimes()
+        {
+            this.locator.Dispose();
+
+            Assert.That(() => this.locator.Dispose(), Throws.Nothing);
+        }
+
+        [Test]
+        public void DisposeShouldDisposeOfTheContainer()
+        {
+            this.locator.Dispose();
+
+            this.container.Received().Dispose();
+        }
+
+        [Test]
+        public void ShouldThrowAnExceptionIfMultipleServicesAreRegisteredForASingleItem()
+        {
+            this.container.Resolve(typeof(IDiscoveryService[]))
+                .Returns(new IDiscoveryService[2]);
+
+            Assert.That(
+                () => this.locator.GetDiscoveryService(),
+                Throws.InstanceOf<InvalidOperationException>()
+                      .And.Message.Contains(nameof(IDiscoveryService)));
+        }
+
+        [Test]
+        public void GetAfterRequestPluginsShouldCheckForDisposed()
+        {
+            this.locator.Dispose();
+
+            Assert.That(
+                () => this.locator.GetAfterRequestPlugins(),
+                Throws.InstanceOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public void GetAfterRequestPluginsShouldReturnTheValueFromTheContainer()
+        {
+            var plugins = new IPostRequestPlugin[0];
+            this.container.Resolve(typeof(IPostRequestPlugin[]))
+                .Returns(plugins);
+
+            IPostRequestPlugin[] result = this.locator.GetAfterRequestPlugins();
+
+            Assert.That(result, Is.SameAs(plugins));
+        }
+
+        [Test]
+        public void GetBeforeRequestPluginsShouldCheckForDisposed()
+        {
+            this.locator.Dispose();
+
+            Assert.That(
+                () => this.locator.GetBeforeRequestPlugins(),
+                Throws.InstanceOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public void GetBeforeRequestPluginsShouldReturnTheValueFromTheContainer()
+        {
+            var plugins = new IPreRequestPlugin[0];
+            this.container.Resolve(typeof(IPreRequestPlugin[]))
+                .Returns(plugins);
+
+            IPreRequestPlugin[] result = this.locator.GetBeforeRequestPlugins();
+
+            Assert.That(result, Is.SameAs(plugins));
+        }
+
+        [Test]
+        public void GetConfigurationServiceShouldCheckForDisposed()
+        {
+            this.locator.Dispose();
+
+            Assert.That(
+                () => this.locator.GetConfigurationService(),
+                Throws.InstanceOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public async Task GetConfigurationServiceShouldGetTheProvidersFromTheContainer()
+        {
+            var configurationProvider = Substitute.For<IConfigurationProvider>();
+            this.container.Resolve(typeof(IEnumerable<IConfigurationProvider>))
+                .Returns(new[] { configurationProvider });
+
+            ConfigurationService result = this.locator.GetConfigurationService();
+            await result.InitializeProviders(new Type[0]);
+
+            await configurationProvider.ReceivedWithAnyArgs().Initialize(null);
+        }
+
+        [Test]
+        public void GetDiscoveryServiceShouldCheckForDisposed()
+        {
+            this.locator.Dispose();
+
+            Assert.That(
+                () => this.locator.GetDiscoveryService(),
+                Throws.InstanceOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public void GetDiscoveryServiceShouldGetTheServiceFromTheContainer()
+        {
+            IDiscoveryService discoveryService = Substitute.For<IDiscoveryService>();
+            this.container.Resolve(typeof(IDiscoveryService[]))
+                .Returns(new[] { discoveryService });
+
+            IDiscoveryService result = this.locator.GetDiscoveryService();
+
+            Assert.That(result, Is.SameAs(discoveryService));
+        }
+
+        [Test]
+        public void GetDiscoveryServiceShouldReturnADefaultRegisteredInstance()
+        {
+            using (var serviceLocator = new ServiceLocator())
+            {
+                IDiscoveryService result = serviceLocator.GetDiscoveryService();
+
+                Assert.That(result, Is.Not.Null);
+            }
+        }
+
+        [Test]
+        public void GetErrorHandlersShouldCheckForDisposed()
+        {
+            this.locator.Dispose();
+
+            Assert.That(
+                () => this.locator.GetErrorHandlers(),
+                Throws.InstanceOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public void GetErrorHandlersShouldReturnTheValueFromTheContainer()
+        {
+            var plugins = new IErrorHandlerPlugin[0];
+            this.container.Resolve(typeof(IErrorHandlerPlugin[]))
+                .Returns(plugins);
+
+            IErrorHandlerPlugin[] result = this.locator.GetErrorHandlers();
+
+            Assert.That(result, Is.SameAs(plugins));
+        }
+
+        [Test]
+        public void GetHtmlTemplateProviderShouldCheckForDisposed()
+        {
+            this.locator.Dispose();
+
+            Assert.That(
+                () => this.locator.GetHtmlTemplateProvider(),
+                Throws.InstanceOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public void GetHtmlTemplateProviderShouldReturnTheValueFromTheContainer()
+        {
+            IHtmlTemplateProvider provider = Substitute.For<IHtmlTemplateProvider>();
+            this.container.Resolve(typeof(IHtmlTemplateProvider[]))
+                .Returns(new[] { provider });
+
+            IHtmlTemplateProvider result = this.locator.GetHtmlTemplateProvider();
+
+            Assert.That(result, Is.SameAs(provider));
+        }
+
+        [Test]
+        public void GetHtmlTemplateProviderShouldReturnADefaultRegisteredInstance()
+        {
+            using (var serviceLocator = new ServiceLocator())
+            {
+                IHtmlTemplateProvider result = serviceLocator.GetHtmlTemplateProvider();
+
+                Assert.That(result, Is.Not.Null);
+            }
+        }
+
+        private class FakeServiceLocator : ServiceLocator
+        {
+            public FakeServiceLocator(IContainer container)
+                : base(container)
+            {
+            }
+
+            internal new bool IsDisposed
+            {
+                get { return base.IsDisposed; }
+            }
+
+            internal new void ThrowIfDisposed()
+            {
+                base.ThrowIfDisposed();
+            }
+        }
+    }
+}
