3d71d737bda28b4f9fededd1647c0cbd6877e563
Samuel Cragg <sam.cragg@outlook.com>
2017-02-17 07:29:10 +0000

    Add unit tests for HtmlTemplatePrivider

---
+++ test/Host.UnitTests/Conversion/HtmlTemplateProviderTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Host.UnitTests.Conversion
+{
+    using Crest.Host.Conversion;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class HtmlTemplateProviderTests
+    {
+        [Test]
+        public void ContentLocationShouldReturnTheLocationAfterTheBodyTag()
+        {
+            var provider = new HtmlTemplateProvider();
+
+            string beforeLocation = provider.Template.Substring(0, provider.ContentLocation);
+
+            Assert.That(beforeLocation, Does.EndWith("<body>"));
+        }
+
+        [Test]
+        public void HintTextShouldReturnANonEmptyValue()
+        {
+            var provider = new HtmlTemplateProvider();
+
+            Assert.That(provider.HintText, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void TemplateShouldReturnANonEmptyValue()
+        {
+            var provider = new HtmlTemplateProvider();
+
+            Assert.That(provider.Template, Is.Not.Null.Or.Empty);
+        }
+    }
+}
