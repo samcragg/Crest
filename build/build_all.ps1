466e29da06c65b0ef40ea6aa8d8242707b761ee8
Samuel Cragg <sam.cragg@outlook.com>
2017-04-14 13:08:39 +0100

    Upgrade build to VS2017

--- build/build_all.ps1
+++ build/build_all.ps1
@@ -2,13 +2,13 @@
 dotnet restore
 
 # Build the project in src as release
-foreach ($project in (dir .\src -Name))
+foreach ($project in (dir .\src -Name -Recurse *.csproj))
 {
 	dotnet build -c Release src\$project
 }
 
 # Build the projects in test
-foreach ($project in (dir .\test -Name))
+foreach ($project in (dir .\test -Name -Recurse *.csproj))
 {
 	dotnet build test\$project
 }
--- build/exclude_dry_ioc_from_stylecop.ps1
+++ build/exclude_dry_ioc_from_stylecop.ps1
@@ -2,11 +2,12 @@ $files = "Container.cs", "ImTools.cs"
 
 foreach ($file in $files)
 {
-    $path = Join-Path $env:userprofile ".nuget/packages/DryIoc.Internal/2.9.0/contentFiles/cs/any"
-    $path = Join-Path $path $file
-    if (!(Test-Path ($path + ".original")))
+    $dir = Join-Path $env:userprofile ".nuget/packages/DryIoc.Internal/2.9.0/contentFiles/cs/any"
+    $path = Join-Path $dir $file
+	$backupName = Join-Path $dir ("../" + $file + ".original")
+    if (!(Test-Path $backupName))
     {
-        Copy-Item $path -Destination ($path + ".original")
+        Copy-Item $path -Destination $backupName
 
         # The Get-Contents/Set-Contents combo seems to nuke newlines (with the
         # encoding set to UTF8) so use the .NET versions instead
