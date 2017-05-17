a5ddf9148f24803042e78ffc6b9488359449589a
Samuel Cragg <sam.cragg@outlook.com>
2017-05-17 18:49:35 +0100

    Add optimised string dictionary

---
+++ src/Crest.Host/StringDictionary.cs
@@ -0,0 +1,171 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An optimized dictionary for storing a small amount of values against a
+    /// string (ignoring the case).
+    /// </summary>
+    /// <typeparam name="T">The type of value to store.</typeparam>
+    /// <remarks>
+    /// <para>
+    /// The dictionary supports fetching, adding and clearing of values - it
+    /// does not support individual key removal/updating.
+    /// </para><para>
+    /// The lookup is done as a linear search, therefore, this class is
+    /// designed for a small number of values. Also, there is no check for
+    /// duplicate keys.
+    /// </para>
+    /// </remarks>
+    internal sealed class StringDictionary<T> : IDictionary<string, T>, IReadOnlyDictionary<string, T>
+    {
+        private const int InitialSize = 4;
+        private string[] keys = new string[InitialSize];
+        private int length;
+        private T[] values = new T[InitialSize];
+
+        /// <inheritdoc />
+        public int Count => this.length;
+
+        /// <inheritdoc />
+        public bool IsReadOnly => false;
+
+        /// <inheritdoc />
+        public ICollection<string> Keys => new ArraySegment<string>(this.keys, 0, this.length);
+
+        /// <inheritdoc />
+        public ICollection<T> Values => new ArraySegment<T>(this.values, 0, this.length);
+
+        /// <inheritdoc />
+        public T this[string key]
+        {
+            get
+            {
+                int index = this.IndexOf(key);
+                return index >= 0 ? this.values[index] : default(T);
+            }
+
+            set => throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        IEnumerable<string> IReadOnlyDictionary<string, T>.Keys => this.Keys;
+
+        /// <inheritdoc />
+        IEnumerable<T> IReadOnlyDictionary<string, T>.Values => this.Values;
+
+        /// <inheritdoc />
+        public void Add(string key, T value)
+        {
+            this.EnsureSpaceToAdd();
+
+            int index = this.length++;
+            this.keys[index] = key;
+            this.values[index] = value;
+        }
+
+        /// <inheritdoc />
+        public void Clear()
+        {
+            Array.Clear(this.keys, 0, this.length);
+            Array.Clear(this.values, 0, this.length);
+            this.length = 0;
+        }
+
+        /// <inheritdoc />
+        public bool ContainsKey(string key)
+        {
+            return this.IndexOf(key) >= 0;
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<KeyValuePair<string, T>> GetEnumerator()
+        {
+            for (int i = 0; i < this.length; i++)
+            {
+                yield return new KeyValuePair<string, T>(this.keys[i], this.values[i]);
+            }
+        }
+
+        /// <inheritdoc />
+        public bool TryGetValue(string key, out T value)
+        {
+            int index = this.IndexOf(key);
+            if (index < 0)
+            {
+                value = default(T);
+                return false;
+            }
+            else
+            {
+                value = this.values[index];
+                return true;
+            }
+        }
+
+        /// <inheritdoc />
+        void ICollection<KeyValuePair<string, T>>.Add(KeyValuePair<string, T> item)
+        {
+            this.Add(item.Key, item.Value);
+        }
+
+        /// <inheritdoc />
+        bool ICollection<KeyValuePair<string, T>>.Contains(KeyValuePair<string, T> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        void ICollection<KeyValuePair<string, T>>.CopyTo(KeyValuePair<string, T>[] array, int arrayIndex)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        bool IDictionary<string, T>.Remove(string key)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        bool ICollection<KeyValuePair<string, T>>.Remove(KeyValuePair<string, T> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        private void EnsureSpaceToAdd()
+        {
+            if (this.length == this.keys.Length)
+            {
+                Array.Resize(ref this.keys, this.keys.Length * 2);
+                Array.Resize(ref this.values, this.values.Length * 2);
+            }
+        }
+
+        private int IndexOf(string key)
+        {
+            for (int i = 0; i < this.length; i++)
+            {
+                if (string.Equals(this.keys[i], key, StringComparison.OrdinalIgnoreCase))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ test/Host.UnitTests/StringDictionaryTests.cs
@@ -0,0 +1,280 @@
+﻿namespace Host.UnitTests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Crest.Host;
+    using FluentAssertions;
+    using Xunit;
+
+    public class StringDictionaryTests
+    {
+        private readonly StringDictionary<int> dictionary = new StringDictionary<int>();
+
+        public sealed class Add : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldAddTheKeyValuePair()
+            {
+                ((ICollection<KeyValuePair<string, int>>)this.dictionary).Add(
+                    new KeyValuePair<string, int>("key", 123));
+
+                this.dictionary["key"].Should().Be(123);
+            }
+            [Fact]
+            public void ShouldResizeToFitAllTheValues()
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    this.dictionary.Add(i.ToString(), i);
+                }
+
+                this.dictionary.Count.Should().Be(6);
+            }
+        }
+
+        public sealed class Clear : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldRemoveAllKeys()
+            {
+                this.dictionary.Add("key", 123);
+
+                this.dictionary.Clear();
+
+                this.dictionary["key"].Should().Be(0);
+            }
+
+            [Fact]
+            public void ShouldResetTheCount()
+            {
+                this.dictionary.Add("key", 123);
+
+                this.dictionary.Clear();
+
+                this.dictionary.Count.Should().Be(0);
+            }
+        }
+
+        public sealed class Contains : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldThrowNotSupportedException()
+            {
+                this.dictionary.Invoking<IDictionary<string, int>>(d => d.Contains(default(KeyValuePair<string, int>)))
+                    .ShouldThrow<NotSupportedException>();
+            }
+        }
+
+        public sealed class CopyTo : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldThrowNotSupportedException()
+            {
+                this.dictionary.Invoking<IDictionary<string, int>>(d => d.CopyTo(null, 0))
+                    .ShouldThrow<NotSupportedException>();
+            }
+        }
+
+        public sealed class ContainsKey : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldReturnFalseIfTheKeyDoesNotExist()
+            {
+                bool result = this.dictionary.ContainsKey("unknown");
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnTrueIfTheKeyExists()
+            {
+                this.dictionary.Add("key", 123);
+
+                bool result = this.dictionary.ContainsKey("key");
+
+                result.Should().BeTrue();
+            }
+        }
+
+        public sealed class Count : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldReturnTheNumberOfAddedValues()
+            {
+                this.dictionary.Add("one", 1);
+                this.dictionary.Add("two", 2);
+
+                this.dictionary.Count.Should().Be(2);
+            }
+        }
+
+        public sealed class GetEnumerator : StringDictionaryTests
+        {
+            [Fact]
+            public void NonGenericEnumeratorShouldReturnTheValues()
+            {
+                this.dictionary.Add("one", 1);
+
+                IEnumerator enumerator = ((IEnumerable)this.dictionary).GetEnumerator();
+
+                enumerator.MoveNext().Should().BeTrue();
+                enumerator.MoveNext().Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnAllTheValues()
+            {
+                this.dictionary.Add("one", 1);
+                this.dictionary.Add("two", 2);
+
+                using (IEnumerator<KeyValuePair<string, int>> enumerator = this.dictionary.GetEnumerator())
+                {
+                    enumerator.MoveNext().Should().BeTrue();
+                    enumerator.Current.Should().Be(new KeyValuePair<string, int>("one", 1));
+
+                    enumerator.MoveNext().Should().BeTrue();
+                    enumerator.Current.Should().Be(new KeyValuePair<string, int>("two", 2));
+
+                    enumerator.MoveNext().Should().BeFalse();
+                }
+            }
+        }
+
+        public sealed class Index : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldIgnoreTheCaseOfTheKey()
+            {
+                this.dictionary.Add("one", 1);
+
+                this.dictionary["ONE"].Should().Be(1);
+            }
+
+            [Fact]
+            public void ShouldReturnTheDefaultValueIfTheKeyDoesNotExist()
+            {
+                this.dictionary["unknown"].Should().Be(0);
+            }
+
+            [Fact]
+            public void ShouldReturnTheValueAssociatedWithTheKey()
+            {
+                this.dictionary.Add("one", 1);
+                this.dictionary.Add("two", 2);
+
+                this.dictionary["two"].Should().Be(2);
+            }
+
+            [Fact]
+            public void ShouldThrowNotSupportedForSettingAValue()
+            {
+                this.dictionary.Invoking(d => d["key"] = 123)
+                    .ShouldThrow<NotSupportedException>();
+            }
+        }
+
+        public sealed class IsReadOnly : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldReturnFalse()
+            {
+                this.dictionary.IsReadOnly.Should().BeFalse();
+            }
+        }
+
+        public sealed class Keys : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldReturnAnEmptyCollectionForAnEmptyDictionary()
+            {
+                this.dictionary.Keys.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldReturnAllTheAddedKeys()
+            {
+                this.dictionary.Add("one", 1);
+                this.dictionary.Add("two", 2);
+
+                this.dictionary.Keys.Should().BeEquivalentTo("one", "two");
+            }
+
+            [Fact]
+            public void ShouldReturnAnIEnumarableValue()
+            {
+                IEnumerable<string> keys =
+                    ((IReadOnlyDictionary<string, int>)this.dictionary).Keys;
+
+                keys.Should().NotBeNull();
+            }
+        }
+
+        public sealed class Remove : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldThrowNotSupportedException()
+            {
+                this.dictionary.Invoking<IDictionary<string, int>>(d => d.Remove("key"))
+                    .ShouldThrow<NotSupportedException>();
+            }
+
+            [Fact]
+            public void ShouldThrowNotSupportedExceptionForKeyValuePairs()
+            {
+                this.dictionary.Invoking<IDictionary<string, int>>(d => d.Remove(default(KeyValuePair<string, int>)))
+                    .ShouldThrow<NotSupportedException>();
+            }
+        }
+
+        public sealed class TryGetValue : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldReturnFalseIfTheKeyDoesNotExist()
+            {
+                bool result = this.dictionary.TryGetValue("unknown", out int value);
+
+                result.Should().BeFalse();
+                value.Should().Be(0);
+            }
+
+            [Fact]
+            public void ShouldReturnTrueIfTheKeyExists()
+            {
+                this.dictionary.Add("key", 123);
+
+                bool result = this.dictionary.TryGetValue("key", out int value);
+
+                result.Should().BeTrue();
+                value.Should().Be(123);
+            }
+        }
+
+        public sealed class Values : StringDictionaryTests
+        {
+            [Fact]
+            public void ShouldReturnAnEmptyCollectionForAnEmptyDictionary()
+            {
+                this.dictionary.Values.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldReturnAllTheAddedKeys()
+            {
+                this.dictionary.Add("one", 1);
+                this.dictionary.Add("two", 2);
+
+                this.dictionary.Values.Should().BeEquivalentTo(1, 2);
+            }
+
+            [Fact]
+            public void ShouldReturnAnIEnumarableValue()
+            {
+                IEnumerable<int> values =
+                    ((IReadOnlyDictionary<string, int>)this.dictionary).Values;
+
+                values.Should().NotBeNull();
+            }
+        }
+    }
+}
