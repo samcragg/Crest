92d59515dd794ef26daf58c4a5f69703a2236620
Samuel Cragg <sam.cragg@outlook.com>
2017-06-05 06:36:57 +0100

    Upgrade solution to VS2017

--- tools/Crest.OpenApi/Properties/AssemblyInfo.cs
+++ tools/Crest.OpenApi/Properties/AssemblyInfo.cs
@@ -3,14 +3,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for
 // full license information.
 
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Samuel Cragg")]
-[assembly: AssemblyProduct("Crest.OpenApi")]
-[assembly: AssemblyTrademark("")]
 
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // NSubstitute
--- tools/OpenApi.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Samuel Cragg")]
-[assembly: AssemblyProduct("OpenApi.UnitTests")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: ComVisible(false)]
