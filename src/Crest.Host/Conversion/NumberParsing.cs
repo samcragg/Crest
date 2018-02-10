13f17fb8e66fc3b093066b23f5e6d1f22f434485
Samuel Cragg <sam.cragg@outlook.com>
2018-02-10 08:37:54 +0000

    Add framework for conversion from string

---
+++ src/Crest.Host/Conversion/NumberParsing.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Conversion
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Contains helper methods used during parsing of numbers.
+    /// </summary>
+    internal static class NumberParsing
+    {
+        /// <summary>
+        /// Calculates the quotient of two 64-bit unsigned integers and also
+        /// returns the remainder in an output parameter.
+        /// </summary>
+        /// <param name="dividend">The dividend.</param>
+        /// <param name="divisor">The divisor.</param>
+        /// <param name="remainder">
+        /// When this method returns, contains the remainder.
+        /// </param>
+        /// <returns>The quotient of the specified numbers.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static ulong DivRem(ulong dividend, ulong divisor, out int remainder)
+        {
+            // See https://github.com/dotnet/coreclr/issues/3439 for why we use
+            // multiplication instead of the modulus operator (basically this
+            // avoids two divide machine instructions)
+            ulong quotient = dividend / divisor;
+            remainder = (int)(dividend - (quotient * divisor));
+            return quotient;
+        }
+
+        /// <summary>
+        /// Calculates the quotient of two 32-bit unsigned integers and also
+        /// returns the remainder in an output parameter.
+        /// </summary>
+        /// <param name="dividend">The dividend.</param>
+        /// <param name="divisor">The divisor.</param>
+        /// <param name="remainder">
+        /// When this method returns, contains the remainder.
+        /// </param>
+        /// <returns>The quotient of the specified numbers.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static uint DivRem(uint dividend, uint divisor, out int remainder)
+        {
+            // See https://github.com/dotnet/coreclr/issues/3439 for why we use
+            // multiplication instead of the modulus operator (basically this
+            // avoids two divide machine instructions)
+            uint quotient = dividend / divisor;
+            remainder = (int)(dividend - (quotient * divisor));
+            return quotient;
+        }
+    }
+}
---
+++ src/Crest.Host/Conversion/ParseResult.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Conversion
+{
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Represents the result of a parse operation.
+    /// </summary>
+    /// <typeparam name="T">The type of the result.</typeparam>
+    [DebuggerDisplay("{Value}")]
+    internal struct ParseResult<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseResult{T}"/> structure.
+        /// </summary>
+        /// <param name="value">The parsed value.</param>
+        /// <param name="length">The number of characters parsed.</param>
+        public ParseResult(T value, int length)
+        {
+            this.Error = null;
+            this.Length = length;
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseResult{T}"/> structure.
+        /// </summary>
+        /// <param name="error">The parsing error.</param>
+        public ParseResult(string error)
+        {
+            this.Error = error;
+            this.Length = 0;
+            this.Value = default;
+        }
+
+        /// <summary>
+        /// Gets the error found during parsing.
+        /// </summary>
+        public string Error { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the parsing operation was a success.
+        /// </summary>
+        public bool IsSuccess => this.Error == null;
+
+        /// <summary>
+        /// Gets the number of characters parsed.
+        /// </summary>
+        public int Length { get; }
+
+        /// <summary>
+        /// Gets the parsed values.
+        /// </summary>
+        public T Value { get; }
+    }
+}
