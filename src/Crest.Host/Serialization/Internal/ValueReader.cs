c66139ed6132a9f19c6631e87a4ce23d61b70f07
Samuel Cragg <sam.cragg@outlook.com>
2018-03-15 06:29:37 +0000

    Add ValueReader abstract class

---
+++ src/Crest.Host/Serialization/Internal/ValueReader.cs
@@ -0,0 +1,264 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Serialization.Internal
+{
+    using System;
+    using System.Globalization;
+    using Crest.Host.Conversion;
+    using SCM = System.ComponentModel;
+
+    /// <summary>
+    /// Reads values from the underlying stream.
+    /// </summary>
+    public abstract class ValueReader
+    {
+        /// <summary>
+        /// Reads a boolean from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public abstract bool ReadBoolean();
+
+        /// <summary>
+        /// Read an 8-bit unsigned integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual byte ReadByte()
+        {
+            return (byte)this.ReadUnsignedInt(byte.MaxValue);
+        }
+
+        /// <summary>
+        /// Reads a Unicode character from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public abstract char ReadChar();
+
+        /// <summary>
+        /// Reads a <see cref="DateTime"/> from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual DateTime ReadDateTime()
+        {
+            ParseResult<DateTime> parsed =
+                DateTimeConverter.TryReadDateTime(this.ReadTrimmedString());
+
+            return this.ProcessResult(parsed, "date/time");
+        }
+
+        /// <summary>
+        /// Reads a <see cref="decimal"/> from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual decimal ReadDecimal()
+        {
+            ParseResult<decimal> parsed =
+                DecimalConverter.TryReadDecimal(this.ReadTrimmedString());
+
+            return this.ProcessResult(parsed, "decimal");
+        }
+
+        /// <summary>
+        /// Reads a double-precision floating-point number from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual double ReadDouble()
+        {
+            ParseResult<double> parsed =
+                DoubleConverter.TryReadDouble(this.ReadTrimmedString());
+
+            return this.ProcessResult(parsed, "double");
+        }
+
+        /// <summary>
+        /// Reads a GUID from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual Guid ReadGuid()
+        {
+            ParseResult<Guid> parsed =
+                GuidConverter.TryReadGuid(this.ReadTrimmedString());
+
+            return this.ProcessResult(parsed, "GUID");
+        }
+
+        /// <summary>
+        /// Reads a 16-bit signed integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual short ReadInt16()
+        {
+            return (short)this.ReadSignedInt(short.MinValue, short.MaxValue);
+        }
+
+        /// <summary>
+        /// Reads a 32-bit signed integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual int ReadInt32()
+        {
+            return (int)this.ReadSignedInt(int.MinValue, int.MaxValue);
+        }
+
+        /// <summary>
+        /// Reads a 64-bit signed integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual long ReadInt64()
+        {
+            return this.ReadSignedInt(long.MinValue, long.MaxValue);
+        }
+
+        /// <summary>
+        /// Attempts to read a token from the stream that indicates there is no
+        /// value.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if there exists no value to read; otherwise, <c>false</c>.
+        /// </returns>
+        public abstract bool ReadNull();
+
+        /// <summary>
+        /// Reads an object from the stream.
+        /// </summary>
+        /// <param name="type">The type of the object to read.</param>
+        /// <returns>The value read from the stream.</returns>
+        public virtual object ReadObject(Type type)
+        {
+            string value = this.ReadString();
+
+            SCM.TypeConverter converter = SCM.TypeDescriptor.GetConverter(type);
+            return converter.ConvertFrom(null, CultureInfo.InvariantCulture, value);
+        }
+
+        /// <summary>
+        /// Reads an 8-bit signed integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        [CLSCompliant(false)]
+        public virtual sbyte ReadSByte()
+        {
+            return (sbyte)this.ReadSignedInt(sbyte.MinValue, sbyte.MaxValue);
+        }
+
+        /// <summary>
+        /// Reads a single-precision floating-point number from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual float ReadSingle()
+        {
+            return (float)this.ReadDouble();
+        }
+
+        /// <summary>
+        /// Reads a string of characters from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public abstract string ReadString();
+
+        /// <summary>
+        /// Reads a <see cref="TimeSpan"/> from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        public virtual TimeSpan ReadTimeSpan()
+        {
+            ParseResult<TimeSpan> parsed =
+                TimeSpanConverter.TryReadTimeSpan(this.ReadTrimmedString());
+
+            return this.ProcessResult(parsed, "timespan");
+        }
+
+        /// <summary>
+        /// Reads a 16-bit unsigned integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        [CLSCompliant(false)]
+        public virtual ushort ReadUInt16()
+        {
+            return (ushort)this.ReadUnsignedInt(ushort.MaxValue);
+        }
+
+        /// <summary>
+        /// Reads a 32-bit unsigned integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        [CLSCompliant(false)]
+        public virtual uint ReadUInt32()
+        {
+            return (uint)this.ReadUnsignedInt(uint.MaxValue);
+        }
+
+        /// <summary>
+        /// Reads a 64-bit unsigned integer from the stream.
+        /// </summary>
+        /// <returns>The value read from the stream.</returns>
+        [CLSCompliant(false)]
+        public virtual ulong ReadUInt64()
+        {
+            return this.ReadUnsignedInt(ulong.MaxValue);
+        }
+
+        /// <summary>
+        /// Gets the current position in the stream, used for reporting errors.
+        /// </summary>
+        /// <returns>A displayable string representing the location.</returns>
+        internal abstract string GetCurrentPosition();
+
+        /// <summary>
+        /// Reads a signed integer.
+        /// </summary>
+        /// <param name="min">The minimum allowed value for the integer.</param>
+        /// <param name="max">The maximum allowed value for the integer.</param>
+        /// <returns>A signed integer.</returns>
+        protected virtual long ReadSignedInt(long min, long max)
+        {
+            ParseResult<long> result = IntegerConverter.TryReadSignedInt(
+                this.ReadTrimmedString(),
+                min,
+                max);
+
+            return this.ProcessResult(result, "integer");
+        }
+
+        /// <summary>
+        /// Reads an unsigned integer.
+        /// </summary>
+        /// <param name="max">The maximum allowed value for the integer.</param>
+        /// <returns>An unsigned integer.</returns>
+        [CLSCompliant(false)]
+        protected virtual ulong ReadUnsignedInt(ulong max)
+        {
+            ParseResult<ulong> result = IntegerConverter.TryReadUnsignedInt(
+                this.ReadTrimmedString(),
+                max);
+
+            return this.ProcessResult(result, "integer");
+        }
+
+        /// <summary>
+        /// Raises an exception if the result of the parsing operation returned
+        /// an error; otherwise, the returns the parsed result.
+        /// </summary>
+        /// <typeparam name="T">The type of the result.</typeparam>
+        /// <param name="result">The result of the parsing operation.</param>
+        /// <param name="type">The display name of the type being parsed.</param>
+        /// <returns>The parsed value.</returns>
+        private protected virtual T ProcessResult<T>(ParseResult<T> result, string type)
+        {
+            if (!result.IsSuccess)
+            {
+                throw new FormatException($"Unable to read {type} at {this.GetCurrentPosition()}: '{result.Error}'.");
+            }
+
+            return result.Value;
+        }
+
+        /// <summary>
+        /// Reads a trimmed string from the stream.
+        /// </summary>
+        /// <returns>A span containing the string.</returns>
+        private protected abstract ReadOnlySpan<char> ReadTrimmedString();
+    }
+}
---
+++ test/Host.UnitTests/Serialization/Internal/ValueReaderTests.cs
@@ -0,0 +1,410 @@
+﻿namespace Host.UnitTests.Serialization.Internal
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using Crest.Host.Serialization.Internal;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ValueReaderTests
+    {
+        private static T ReadValue<T>(string contents, Func<ValueReader, T> readMethod)
+        {
+            var reader = new FakeValueReader
+            {
+                CurrentContents = contents
+            };
+
+            return readMethod(reader);
+        }
+
+        public sealed class ReadByte : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(0)]
+            [InlineData(byte.MaxValue)]
+            public void ShouldReadIntegerLimits(byte value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                byte result = ReadValue(stringValue, r => r.ReadByte());
+
+                result.Should().Be(value);
+            }
+
+            [Theory]
+            [InlineData(-1)]
+            [InlineData(byte.MaxValue + 1)]
+            public void ShouldThrowForOutOfRange(int value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                Action action = () => ReadValue(stringValue, r => r.ReadByte());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadDateTime : ValueReaderTests
+        {
+            [Fact]
+            public void ShouldReadIsoDateTimes()
+            {
+                DateTime result = ReadValue("2017-12-31T13:14:15-01:00", r => r.ReadDateTime());
+
+                result.Should().Be(new DateTime(2017, 12, 31, 14, 14, 15, DateTimeKind.Utc));
+            }
+
+            [Fact]
+            public void ShouldThrowForInvalidDates()
+            {
+                Action action = () => ReadValue("invalid", r => r.ReadDateTime());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadDecimal : ValueReaderTests
+        {
+            [Theory]
+            [InlineData("-79228162514264337593543950335")]
+            [InlineData("0")]
+            [InlineData("79228162514264337593543950335")]
+            public void ShouldReadTheLimits(string value)
+            {
+                decimal expected = decimal.Parse(value, CultureInfo.InvariantCulture);
+
+                decimal result = ReadValue(value, r => r.ReadDecimal());
+
+                result.Should().Be(expected);
+            }
+        }
+
+        public sealed class ReadDouble : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(double.MinValue)]
+            [InlineData(0)]
+            [InlineData(double.MaxValue)]
+            public void ShouldReadTheLimits(double value)
+            {
+                string stringValue = value.ToString("r", CultureInfo.InvariantCulture);
+
+                double result = ReadValue(stringValue, r => r.ReadDouble());
+
+                result.Should().Be(value);
+            }
+        }
+
+        public sealed class ReadGuid : ValueReaderTests
+        {
+            [Fact]
+            public void ShouldReadGuidStrings()
+            {
+                const string GuidString = "7DBE08AD-4460-453F-ABF6-CA6E25235083";
+
+                Guid result = ReadValue(GuidString, r => r.ReadGuid());
+
+                result.Should().Be(new Guid(GuidString));
+            }
+
+            [Fact]
+            public void ShouldThrowForInvalidGuids()
+            {
+                Action action = () => ReadValue("invalid", r => r.ReadGuid());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadInt16 : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(short.MinValue)]
+            [InlineData(0)]
+            [InlineData(short.MaxValue)]
+            public void ShouldReadIntegerLimits(short value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                short result = ReadValue(stringValue, r => r.ReadInt16());
+
+                result.Should().Be(value);
+            }
+
+            [Theory]
+            [InlineData(short.MinValue - 1)]
+            [InlineData(short.MaxValue + 1)]
+            public void ShouldThrowForOutOfRange(int value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                Action action = () => ReadValue(stringValue, r => r.ReadInt16());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadInt32 : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(int.MinValue)]
+            [InlineData(0)]
+            [InlineData(int.MaxValue)]
+            public void ShouldReadIntegerLimits(int value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                int result = ReadValue(stringValue, r => r.ReadInt32());
+
+                result.Should().Be(value);
+            }
+
+            [Theory]
+            [InlineData(int.MinValue - 1L)]
+            [InlineData(int.MaxValue + 1L)]
+            public void ShouldThrowForOutOfRange(long value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                Action action = () => ReadValue(stringValue, r => r.ReadInt32());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadInt64 : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(long.MinValue)]
+            [InlineData(0)]
+            [InlineData(long.MaxValue)]
+            public void ShouldReadIntegerLimits(long value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                long result = ReadValue(stringValue, r => r.ReadInt64());
+
+                result.Should().Be(value);
+            }
+        }
+
+        public sealed class ReadObject : ValueReaderTests
+        {
+            [Fact]
+            public void ShouldUseCustomTypeConverters()
+            {
+                object result = ReadValue(" string ", r => r.ReadObject(typeof(CustomValue)));
+
+                result.Should().BeOfType<CustomValue>()
+                      .Which.Value.Should().Be(" string ");
+            }
+
+            [TypeConverter(typeof(CustomValueConverter))]
+            private class CustomValue
+            {
+                public CustomValue(string value)
+                {
+                    this.Value = value;
+                }
+
+                public string Value { get; }
+            }
+
+            private class CustomValueConverter : TypeConverter
+            {
+                public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+                {
+                    return new CustomValue((string)value);
+                }
+            }
+        }
+
+        public sealed class ReadSByte : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(sbyte.MinValue)]
+            [InlineData(0)]
+            [InlineData(sbyte.MaxValue)]
+            public void ShouldReadIntegerLimits(sbyte value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                sbyte result = ReadValue(stringValue, r => r.ReadSByte());
+
+                result.Should().Be(value);
+            }
+
+            [Theory]
+            [InlineData(sbyte.MinValue - 1)]
+            [InlineData(sbyte.MaxValue + 1)]
+            public void ShouldThrowForOutOfRange(int value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                Action action = () => ReadValue(stringValue, r => r.ReadSByte());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadSingle : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(float.MinValue)]
+            [InlineData(0)]
+            [InlineData(float.MaxValue)]
+            public void ShouldReadTheLimits(float value)
+            {
+                string stringValue = value.ToString("r", CultureInfo.InvariantCulture);
+
+                float result = ReadValue(stringValue, r => r.ReadSingle());
+
+                result.Should().Be(value);
+            }
+        }
+
+        public sealed class ReadTimeSpan : ValueReaderTests
+        {
+            [Fact]
+            public void ShouldReadDotNetTimeSpans()
+            {
+                TimeSpan result = ReadValue("1.2:03:04.0050000", r => r.ReadTimeSpan());
+
+                result.Should().BeCloseTo(new TimeSpan(1, 2, 3, 4, 5));
+            }
+
+            [Fact]
+            public void ShouldReadIsoTimeSpans()
+            {
+                TimeSpan result = ReadValue("P1DT2H3M4.005S", r => r.ReadTimeSpan());
+
+                result.Should().BeCloseTo(new TimeSpan(1, 2, 3, 4, 5));
+            }
+
+            [Fact]
+            public void ShouldThrowForInvalidStrings()
+            {
+                Action action = () => ReadValue("invalid", r => r.ReadTimeSpan());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadUInt16 : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(0)]
+            [InlineData(ushort.MaxValue)]
+            public void ShouldReadIntegerLimits(ushort value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                ushort result = ReadValue(stringValue, r => r.ReadUInt16());
+
+                result.Should().Be(value);
+            }
+
+            [Theory]
+            [InlineData(-1)]
+            [InlineData(ushort.MaxValue + 1)]
+            public void ShouldThrowForOutOfRange(int value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                Action action = () => ReadValue(stringValue, r => r.ReadUInt16());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadUInt32 : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(0)]
+            [InlineData(uint.MaxValue)]
+            public void ShouldReadIntegerLimits(uint value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                uint result = ReadValue(stringValue, r => r.ReadUInt32());
+
+                result.Should().Be(value);
+            }
+
+            [Theory]
+            [InlineData(-1)]
+            [InlineData(uint.MaxValue + 1L)]
+            public void ShouldThrowForOutOfRange(long value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                Action action = () => ReadValue(stringValue, r => r.ReadUInt32());
+
+                action.Should().Throw<FormatException>()
+                      .WithMessage("*" + FakeValueReader.Position + "*");
+            }
+        }
+
+        public sealed class ReadUInt64 : ValueReaderTests
+        {
+            [Theory]
+            [InlineData(0)]
+            [InlineData(ulong.MaxValue)]
+            public void ShouldReadIntegerLimits(ulong value)
+            {
+                string stringValue = value.ToString(CultureInfo.InvariantCulture);
+
+                ulong result = ReadValue(stringValue, r => r.ReadUInt64());
+
+                result.Should().Be(value);
+            }
+        }
+
+        private sealed class FakeValueReader : ValueReader
+        {
+            internal const string Position = "current position";
+
+            internal string CurrentContents { get; set; }
+
+            public override bool ReadBoolean()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override char ReadChar()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override bool ReadNull()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string ReadString()
+            {
+                return this.CurrentContents;
+            }
+
+            internal override string GetCurrentPosition()
+            {
+                return Position;
+            }
+
+            private protected override ReadOnlySpan<char> ReadTrimmedString()
+            {
+                return this.CurrentContents.AsSpan();
+            }
+        }
+    }
+}
