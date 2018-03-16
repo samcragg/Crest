35ff6b63c97b0ac2e75bfd222e9d797f5a6c2d1e
Samuel Cragg <sam.cragg@outlook.com>
2018-03-16 07:14:25 +0000

    Add a reader for URL encoded serialization

---
+++ src/Crest.Host/Serialization/UrlEncodedStreamReader.FormParser.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Serialization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    /// <content>
+    /// Contains the nested <see cref="FormParser"/> struct.
+    /// </content>
+    internal sealed partial class UrlEncodedStreamReader
+    {
+        private struct FormParser
+        {
+            private StringBuffer buffer;
+            private bool inValue;
+            private string key;
+            private List<Pair> pairs;
+
+            public FormParser(StreamIterator iterator, StringBuffer buffer)
+                : this()
+            {
+                this.buffer = buffer;
+                this.key = string.Empty;
+                this.pairs = new List<Pair>();
+
+                while (iterator.MoveNext())
+                {
+                    this.AppendCharacter(iterator);
+                }
+
+                // Add the last pair. This also ensure that if the input stream
+                // is empty we at least return one empty pair, making things
+                // easier for the reader
+                this.pairs.Add(new Pair(this.key, this.buffer.ToString()));
+
+                // Sort the results so that nested properties can be read all
+                // together (i.e. NestedClass.Property1 and NestedClass.Property2
+                // can be read while building the NestedClass object)
+                this.pairs.Sort();
+            }
+
+            public IReadOnlyList<Pair> Pairs => this.pairs;
+
+            private static bool AddHexValue(char c, ref uint value)
+            {
+                uint digit = (uint)(c - '0');
+                if (digit > 9)
+                {
+                    digit = (uint)(c - 'A');
+                    if (digit > 5)
+                    {
+                        digit = (uint)(c - 'a');
+                        if (digit > 5)
+                        {
+                            return false;
+                        }
+                    }
+
+                    digit += 10;
+                }
+
+                value += digit;
+                return true;
+            }
+
+            private static char DecodeHexPair(StreamIterator iterator)
+            {
+                if (iterator.MoveNext())
+                {
+                    char first = iterator.Current;
+                    if (iterator.MoveNext())
+                    {
+                        uint value = 0;
+                        if (!AddHexValue(first, ref value))
+                        {
+                            throw new FormatException("Invalid hexadecimal character");
+                        }
+
+                        value <<= 4;
+                        if (!AddHexValue(iterator.Current, ref value))
+                        {
+                            throw new FormatException("Invalid hexadecimal character");
+                        }
+
+                        return (char)value;
+                    }
+                }
+
+                throw new FormatException("Expected percent-encoded value");
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private void AppendCharacter(StreamIterator iterator)
+            {
+                char c = iterator.Current;
+                switch (c)
+                {
+                    case '&':
+                        this.pairs.Add(new Pair(this.key, this.buffer.ToString()));
+
+                        this.inValue = false;
+                        this.key = string.Empty;
+                        this.buffer.Clear();
+                        break;
+
+                    case '=':
+                        if (this.inValue)
+                        {
+                            goto default;
+                        }
+                        else
+                        {
+                            this.inValue = true;
+                            this.key = this.buffer.ToString();
+                            this.buffer.Clear();
+                        }
+
+                        break;
+
+                    case '+':
+                        this.buffer.Append(' ');
+                        break;
+
+                    case '%':
+                        this.buffer.Append(DecodeHexPair(iterator));
+                        break;
+
+                    default:
+                        this.buffer.Append(c);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Serialization/UrlEncodedStreamReaderTests.cs
@@ -0,0 +1,191 @@
+﻿namespace Host.UnitTests.Serialization
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Crest.Host.Serialization;
+    using FluentAssertions;
+    using Xunit;
+
+    public class UrlEncodedStreamReaderTests
+    {
+        private static T ReadValue<T>(string text, Func<UrlEncodedStreamReader, T> readMethod)
+        {
+            T result = default;
+            WithReader(text, r =>
+            {
+                result = readMethod(r);
+            });
+
+            return result;
+        }
+
+        private static void WithReader(string text, Action<UrlEncodedStreamReader> action)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            using (var stream = new MemoryStream(bytes, writable: false))
+            {
+                var reader = new UrlEncodedStreamReader(stream);
+                action(reader);
+            }
+        }
+
+        public sealed class Constructor : UrlEncodedStreamReaderTests
+        {
+            [Theory]
+            [InlineData("%")]
+            [InlineData("%1")]
+            [InlineData("%1G")]
+            [InlineData("%g1")]
+            public void ShouldThrowForInvalidEscapeSequences(string sequence)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(sequence);
+                using (var stream = new MemoryStream(bytes, writable: false))
+                {
+                    Action action = () => new UrlEncodedStreamReader(stream);
+
+                    action.Should().Throw<FormatException>();
+                }
+            }
+        }
+
+        public sealed class GetCurrentPosition : UrlEncodedStreamReaderTests
+        {
+            [Fact]
+            public void ShouldReturnTheKeyInformation()
+            {
+                WithReader("keyName=value", r =>
+                {
+                    r.ReadString();
+
+                    string position = r.GetCurrentPosition();
+
+                    position.Should().Contain("keyName");
+                });
+            }
+        }
+
+        public sealed class MoveToNextSibling : UrlEncodedStreamReaderTests
+        {
+            [Fact]
+            public void ShouldReturnFalseIfTheNextPartDoesNotHaveTheSamePrefix()
+            {
+                WithReader("one.two=x&three.four=x", r =>
+                {
+                    r.MoveToChildren();
+
+                    bool result = r.MoveToNextSibling();
+
+                    result.Should().BeFalse();
+                });
+            }
+
+            [Fact]
+            public void ShouldReturnFalseWhenAtTheEnd()
+            {
+                WithReader("one=value", r =>
+                {
+                    bool result = r.MoveToNextSibling();
+
+                    result.Should().BeFalse();
+                });
+            }
+        }
+
+        public sealed class ReadBoolean : UrlEncodedStreamReaderTests
+        {
+            [Fact]
+            public void ShouldReturnFalseForTheFalseToken()
+            {
+                bool result = ReadValue("key=false", r => r.ReadBoolean());
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnTrueForTheTrueToken()
+            {
+                bool result = ReadValue("key=true", r => r.ReadBoolean());
+
+                result.Should().BeTrue();
+            }
+
+            [Fact]
+            public void ShouldThrowForInvalidTokens()
+            {
+                Action action = () => ReadValue("key=invalid", r => r.ReadBoolean());
+
+                // Make sure the exception includes where it was
+                action.Should().Throw<FormatException>().WithMessage("*key*");
+            }
+        }
+
+        public sealed class ReadChar : UrlEncodedStreamReaderTests
+        {
+            [Theory]
+            [InlineData("X", 'X')]
+            [InlineData("%3d", '=')]
+            [InlineData("%3D", '=')]
+            public void ShouldReturnTheCharacter(string value, char expected)
+            {
+                char result = ReadValue("key=" + value, r => r.ReadChar());
+
+                result.Should().Be(expected);
+            }
+
+            [Fact]
+            public void ShouldThrowForMissingCharacters()
+            {
+                Action action = () => ReadValue("key=", r => r.ReadChar());
+
+                action.Should().Throw<FormatException>();
+            }
+
+            [Fact]
+            public void ShouldThrowForTooManyCharacters()
+            {
+                Action action = () => ReadValue("key=invalid", r => r.ReadChar());
+
+                action.Should().Throw<FormatException>();
+            }
+        }
+
+        public sealed class ReadNull : UrlEncodedStreamReaderTests
+        {
+            [Fact]
+            public void ShouldReturnFalseIfTheTokenIsNotNull()
+            {
+                bool result = ReadValue("key=value", r => r.ReadNull());
+
+                result.Should().Be(false);
+            }
+
+            [Fact]
+            public void ShouldReturnTrueIfTheTokenIsNull()
+            {
+                bool result = ReadValue("key=null", r => r.ReadNull());
+
+                result.Should().Be(true);
+            }
+        }
+
+        public sealed class ReadString : XmlStreamReaderTests
+        {
+            [Fact]
+            public void ShouldReadEmptyValues()
+            {
+                string result = ReadValue("key=", r => r.ReadString());
+
+                result.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldReadTheValue()
+            {
+                string result = ReadValue("key=string+value", r => r.ReadString());
+
+                result.Should().Be("string value");
+            }
+        }
+    }
+}
