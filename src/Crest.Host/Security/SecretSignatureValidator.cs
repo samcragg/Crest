8a847147f14e03f4ee0941d968d732729e68072d
Samuel Cragg <sam.cragg@outlook.com>
2018-03-27 22:34:38 +0100

    Validate signatures generated from shared secrets

---
+++ src/Crest.Host/Security/SecretSignatureValidator.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Security
+{
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// Allows the validation of shared secret signatures.
+    /// </summary>
+    internal sealed class SecretSignatureValidator : ISignatureValidator
+    {
+        private readonly SecurityKeyCache keys;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SecretSignatureValidator"/> class.
+        /// </summary>
+        /// <param name="keys">The cache containing the security keys.</param>
+        public SecretSignatureValidator(SecurityKeyCache keys)
+        {
+            this.keys = keys;
+        }
+
+        /// <inheritdoc />
+        public string AlgorithmPrefix => "HS";
+
+        /// <inheritdoc />
+        public bool IsValid(byte[] data, byte[] signature, HashAlgorithmName algorithm)
+        {
+            HMAC hmac = GetAlgorithm(algorithm);
+            if ((hmac == null) || (signature == null))
+            {
+                return false;
+            }
+
+            using (hmac)
+            {
+                if (signature.Length != (hmac.HashSize / 8))
+                {
+                    return false;
+                }
+
+                foreach (byte[] secret in this.keys.GetSecretKeys())
+                {
+                    hmac.Key = secret;
+                    byte[] hash = hmac.ComputeHash(data);
+                    if (ArraysAreEqual(signature, hash))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool ArraysAreEqual(byte[] a, byte[] b)
+        {
+            // We've checked for null and length in the caller
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static HMAC GetAlgorithm(HashAlgorithmName name)
+        {
+            switch (name.Name)
+            {
+                case "SHA256":
+                    return new HMACSHA256();
+
+                case "SHA384":
+                    return new HMACSHA384();
+
+                case "SHA512":
+                    return new HMACSHA512();
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Security/SecretSignatureValidatorTests.cs
@@ -0,0 +1,115 @@
+﻿namespace Host.UnitTests.Security
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+    using Crest.Host.Security;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    public class SecretSignatureValidatorTests
+    {
+        private readonly SecurityKeyCache cache;
+        private readonly byte[] secretKey = new byte[] { 1, 2, 3 };
+        private readonly SecretSignatureValidator validator;
+
+        public SecretSignatureValidatorTests()
+        {
+            this.cache = Substitute.For<SecurityKeyCache>(new object[] { new ISecurityKeyProvider[0] });
+            this.validator = new SecretSignatureValidator(this.cache);
+            this.cache.GetSecretKeys().Returns(new[] { this.secretKey });
+        }
+
+        public sealed class AlgorithmPrefix : SecretSignatureValidatorTests
+        {
+            [Fact]
+            public void ShouldReturnHS()
+            {
+                this.validator.AlgorithmPrefix
+                    .Should().Be("HS");
+            }
+        }
+
+        public sealed class IsValid : SecretSignatureValidatorTests
+        {
+            [Fact]
+            public void ShouldHandleSHA384()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data, b => new HMACSHA384(b));
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA384);
+
+                result.Should().BeTrue();
+            }
+
+            [Fact]
+            public void ShouldHandleSHA512()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data, b => new HMACSHA512(b));
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA512);
+
+                result.Should().BeTrue();
+            }
+
+            [Fact]
+            public void ShouldReturnFalseForIncorrectSizedSignatures()
+            {
+                bool result = this.validator.IsValid(new byte[0], new byte[1], HashAlgorithmName.SHA256);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnFalseForInvalidSignatures()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data);
+                signature[0] = (byte)~signature[0];
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA256);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnFalseForNullSignatures()
+            {
+                bool result = this.validator.IsValid(new byte[0], null, HashAlgorithmName.SHA256);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnFalseForUnknownAlgorithms()
+            {
+                bool result = this.validator.IsValid(new byte[0], new byte[128 / 8], HashAlgorithmName.MD5);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnTrueForValidSignatures()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data);
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA256);
+
+                result.Should().BeTrue();
+            }
+
+            private byte[] SignData(byte[] data, Func<byte[], HashAlgorithm> factory = null)
+            {
+                factory = factory ?? (b => new HMACSHA256(b));
+                using (HashAlgorithm hmac = factory(this.secretKey))
+                {
+                    return hmac.ComputeHash(data);
+                }
+            }
+        }
+    }
+}
