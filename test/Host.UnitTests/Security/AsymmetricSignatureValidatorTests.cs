9d22e24f13a7e6c1b389818a95404f6ff793c524
Samuel Cragg <sam.cragg@outlook.com>
2018-03-28 06:49:23 +0100

    Test all supported hashing methods

--- test/Host.UnitTests/Security/AsymmetricSignatureValidatorTests.cs
+++ test/Host.UnitTests/Security/AsymmetricSignatureValidatorTests.cs
@@ -14,7 +14,29 @@
         public sealed class IsValid : AsymmetricSignatureValidatorTests
         {
             [Fact]
-            public void ShouldHashTheData()
+            public void ShouldHashTheDataWith256()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test Data");
+                byte[] sha256Hash = Convert.FromBase64String("vP5nFypvQHnWn+Lyeplg+dYu2uL81LtaYGwuu3SzumU=");
+
+                this.validator.IsValid(data, new byte[0], HashAlgorithmName.SHA256);
+
+                this.validator.Hash.Should().Equal(sha256Hash);
+            }
+
+            [Fact]
+            public void ShouldHashTheDataWith384()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test Data");
+                byte[] sha384Hash = Convert.FromBase64String("GFAOZC+qkzI9i5TiiKsPvoNaQD8N3y6grwRTeG0/JhYjfYXXQhTrIHytKacL2dTr");
+
+                this.validator.IsValid(data, new byte[0], HashAlgorithmName.SHA384);
+
+                this.validator.Hash.Should().Equal(sha384Hash);
+            }
+
+            [Fact]
+            public void ShouldHashTheDataWith512()
             {
                 byte[] data = Encoding.UTF8.GetBytes("Test Data");
                 byte[] sha512Hash = Convert.FromBase64String("Q55M7tkxL+8uVUBCw9J9asMdqc9yuoZrqbDgAyjQYoB5dIK/LNAH4ICCltsLmHtz/h+VPpfiWIMmO5eDUTwpSQ==");
