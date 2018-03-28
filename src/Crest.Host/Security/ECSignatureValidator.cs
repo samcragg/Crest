204c911bbb389aa19f1d58f54c00c05914e4fc2a
Samuel Cragg <sam.cragg@outlook.com>
2018-03-28 06:12:59 +0100

    Validate Elliptic Curve signatures

---
+++ src/Crest.Host/Security/ECSignatureValidator.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Security
+{
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// Allows the validation of Elliptic Curve signatures.
+    /// </summary>
+    internal sealed class ECSignatureValidator : AsymmetricSignatureValidator
+    {
+        private readonly SecurityKeyCache keys;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ECSignatureValidator"/> class.
+        /// </summary>
+        /// <param name="keys">The cache containing the security keys.</param>
+        public ECSignatureValidator(SecurityKeyCache keys)
+        {
+            this.keys = keys;
+        }
+
+        /// <inheritdoc />
+        public override string AlgorithmPrefix => "ES";
+
+        /// <inheritdoc />
+        protected override bool ValidateHash(byte[] hash, byte[] signature, HashAlgorithmName algorithm)
+        {
+            using (var ec = ECDsa.Create())
+            {
+                foreach (ECParameters parameters in this.keys.GetECParameters())
+                {
+                    ec.ImportParameters(parameters);
+                    if (ec.VerifyHash(hash, signature))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/Host.UnitTests/Security/ECSignatureValidatorTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Host.UnitTests.Security
+{
+    using System.Security.Cryptography;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using Crest.Host.Security;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    public class ECSignatureValidatorTests
+    {
+        private readonly SecurityKeyCache cache;
+        private readonly ECSignatureValidator validator;
+
+        public ECSignatureValidatorTests()
+        {
+            this.cache = Substitute.For<SecurityKeyCache>(new object[] { new ISecurityKeyProvider[0] });
+            this.validator = new ECSignatureValidator(this.cache);
+
+            using (X509Certificate2 cert = CertificateHelper.GetCertificate("TestEcCert.pfx"))
+            {
+                this.cache.GetECParameters().Returns(new[] { cert.GetECDsaPublicKey().ExportParameters(false) });
+            }
+        }
+
+        public sealed class AlgorithmPrefix : ECSignatureValidatorTests
+        {
+            [Fact]
+            public void ShouldReturnES()
+            {
+                this.validator.AlgorithmPrefix
+                    .Should().Be("ES");
+            }
+        }
+
+        public sealed class IsValid : ECSignatureValidatorTests
+        {
+            [Fact]
+            public void ShouldReturnFalseForInvalidSignatures()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data);
+                signature[0] = (byte)~signature[0];
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA256);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnTrueForValidSignatures()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data);
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA256);
+
+                result.Should().BeTrue();
+            }
+
+            private static byte[] SignData(byte[] data)
+            {
+                using (X509Certificate2 cert = CertificateHelper.GetCertificate("TestEcCert.pfx"))
+                using (ECDsa ec = cert.GetECDsaPrivateKey())
+                {
+                    return ec.SignData(data, HashAlgorithmName.SHA256);
+                }
+            }
+        }
+    }
+}
