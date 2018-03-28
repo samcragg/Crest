3bb948c01fcab8c7d1cddee679c281ef4e09fb0c
Samuel Cragg <sam.cragg@outlook.com>
2018-03-28 06:10:11 +0100

    Validate RSA signatures

---
+++ src/Crest.Host/Security/RsaSignatureValidator.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Security
+{
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// Allows the validation of RSA signatures.
+    /// </summary>
+    internal sealed class RsaSignatureValidator : AsymmetricSignatureValidator
+    {
+        private readonly SecurityKeyCache keys;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RsaSignatureValidator"/> class.
+        /// </summary>
+        /// <param name="keys">The cache containing the security keys.</param>
+        public RsaSignatureValidator(SecurityKeyCache keys)
+        {
+            this.keys = keys;
+        }
+
+        /// <inheritdoc />
+        public override string AlgorithmPrefix => "RS";
+
+        /// <inheritdoc />
+        protected override bool ValidateHash(byte[] hash, byte[] signature, HashAlgorithmName algorithm)
+        {
+            using (var rsa = RSA.Create())
+            {
+                foreach (RSAParameters parameters in this.keys.GetRsaParameters())
+                {
+                    rsa.ImportParameters(parameters);
+                    if (rsa.VerifyHash(hash, signature, algorithm, RSASignaturePadding.Pkcs1))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/Host.UnitTests/Security/RsaSignatureValidatorTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Host.UnitTests.Security
+{
+    using System.Security.Cryptography;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using Crest.Host.Security;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    public class RsaSignatureValidatorTests
+    {
+        private readonly SecurityKeyCache cache;
+        private readonly RsaSignatureValidator validator;
+
+        public RsaSignatureValidatorTests()
+        {
+            this.cache = Substitute.For<SecurityKeyCache>(new object[] { new ISecurityKeyProvider[0] });
+            this.validator = new RsaSignatureValidator(this.cache);
+
+            using (X509Certificate2 cert = CertificateHelper.GetCertificate("TestRsaCert.pfx"))
+            {
+                this.cache.GetRsaParameters().Returns(new[] { cert.GetRSAPublicKey().ExportParameters(false) });
+            }
+        }
+
+        public sealed class AlgorithmPrefix : RsaSignatureValidatorTests
+        {
+            [Fact]
+            public void ShouldReturnRS()
+            {
+                this.validator.AlgorithmPrefix
+                    .Should().Be("RS");
+            }
+        }
+
+        public sealed class IsValid : RsaSignatureValidatorTests
+        {
+            [Fact]
+            public void ShouldReturnFalseForInvalidSignatures()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data);
+                signature[0] = (byte)~signature[0];
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA256);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnTrueForValidSignatures()
+            {
+                byte[] data = Encoding.UTF8.GetBytes("Test");
+                byte[] signature = SignData(data);
+
+                bool result = this.validator.IsValid(data, signature, HashAlgorithmName.SHA256);
+
+                result.Should().BeTrue();
+            }
+
+            private static byte[] SignData(byte[] data)
+            {
+                using (X509Certificate2 cert = CertificateHelper.GetCertificate("TestRsaCert.pfx"))
+                using (RSA rsa = cert.GetRSAPrivateKey())
+                {
+                    return rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                }
+            }
+        }
+    }
+}
