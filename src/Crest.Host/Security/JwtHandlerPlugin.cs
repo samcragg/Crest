6d8981a5aa14bb612422b0d71cf8a7dd9eea452a
Samuel Cragg <sam.cragg@outlook.com>
2018-04-03 06:40:34 +0100

    Validate JWT authorization headers

---
+++ src/Crest.Host/Security/JwtHandlerPlugin.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Security
+{
+    using System;
+    using System.Net;
+    using System.Reflection;
+    using System.Security.Claims;
+    using System.Security.Principal;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Crest.Abstractions;
+    using Crest.Core;
+    using Crest.Host.Logging;
+
+    /// <summary>
+    /// Inspects the authentication token and injects it into the pipeline.
+    /// </summary>
+    internal sealed class JwtHandlerPlugin : IPreRequestPlugin
+    {
+        private const string AuthorizationHeader = "Authorization";
+        private const string BearerPrefix = "Bearer ";
+
+        private static readonly Task<IResponseData> ContinueRequest =
+            Task.FromResult<IResponseData>(null);
+
+        private static readonly AsyncLocal<ClaimsPrincipal> CurrentPrincipal =
+            new AsyncLocal<ClaimsPrincipal>();
+
+        private static readonly ILog Logger = LogProvider.For<JwtHandlerPlugin>();
+        private static readonly Task<IResponseData> UnauthorizedRequest;
+
+        private readonly IScopedServiceRegister serviceRegister;
+        private readonly JwtSignatureVerifier signatureVerifier;
+        private readonly JwtValidator validator;
+
+        static JwtHandlerPlugin()
+        {
+            byte[] unauthorizedText = Encoding.UTF8.GetBytes("401 Unauthorized");
+            var unauthorizedResponse = new ResponseData(
+                "text/plain",
+                (int)HttpStatusCode.Unauthorized,
+                stream => stream.WriteAsync(unauthorizedText, 0, unauthorizedText.Length));
+
+            unauthorizedResponse.Headers.Add("WWW-Authenticate", BearerPrefix);
+            UnauthorizedRequest = Task.FromResult<IResponseData>(unauthorizedResponse);
+
+            ClaimsPrincipal.ClaimsPrincipalSelector = () => CurrentPrincipal.Value;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JwtHandlerPlugin"/> class.
+        /// </summary>
+        /// <param name="register">Used to register the <see cref="IPrincipal"/>.</param>
+        /// <param name="validator">Used to validate the JWT payload.</param>
+        /// <param name="signature">Used to validate the JWT signature.</param>
+        public JwtHandlerPlugin(IScopedServiceRegister register, JwtValidator validator, JwtSignatureVerifier signature)
+        {
+            this.serviceRegister = register;
+            this.validator = validator;
+            this.signatureVerifier = signature;
+        }
+
+        /// <inheritdoc />
+        public int Order => 1;
+
+        /// <inheritdoc />
+        public Task<IResponseData> ProcessAsync(IRequestData request)
+        {
+            if (request.Headers.TryGetValue(AuthorizationHeader, out string authorization))
+            {
+                if (!this.ValidateBearerToken(authorization, out ClaimsPrincipal principal))
+                {
+                    return UnauthorizedRequest;
+                }
+
+                this.serviceRegister.UseInstance(typeof(IPrincipal), principal);
+            }
+            else if (!IsAnonymous(request.Handler))
+            {
+                Logger.InfoFormat("Access to {method} requires an authorization header", request.Handler.Name);
+                return UnauthorizedRequest;
+            }
+
+            return ContinueRequest;
+        }
+
+        private static bool IsAnonymous(MethodInfo handler)
+        {
+            return handler.GetCustomAttribute<AllowAnonymousAttribute>() != null;
+        }
+
+        private bool ValidateBearerToken(string authorization, out ClaimsPrincipal principal)
+        {
+            if (!authorization.StartsWith(BearerPrefix, StringComparison.Ordinal))
+            {
+                Logger.Warn("Authorization header must start with " + BearerPrefix);
+                principal = null;
+            }
+            else if (!this.signatureVerifier.IsSignatureValid(
+                authorization.Substring(BearerPrefix.Length),
+                out byte[] payload))
+            {
+                principal = null;
+            }
+            else
+            {
+                principal = this.validator.GetValidClaimsPrincipal(payload);
+            }
+
+            if (principal == null)
+            {
+                return false;
+            }
+
+            CurrentPrincipal.Value = principal;
+            return true;
+        }
+    }
+}
---
+++ test/Host.UnitTests/Security/JwtHandlerPluginTests.cs
@@ -0,0 +1,163 @@
+﻿namespace Host.UnitTests.Security
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Security.Claims;
+    using System.Security.Principal;
+    using System.Threading.Tasks;
+    using Crest.Abstractions;
+    using Crest.Core;
+    using Crest.Host.Security;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    public class JwtHandlerPluginTests
+    {
+        private const int Unauthorized = 401;
+        private readonly JwtHandlerPlugin plugin;
+        private readonly IScopedServiceRegister register;
+        private readonly JwtValidator validator;
+        private readonly JwtSignatureVerifier verifier;
+
+        private JwtHandlerPluginTests()
+        {
+            this.register = Substitute.For<IScopedServiceRegister>();
+            this.validator = Substitute.For<JwtValidator>(null, null);
+            this.verifier = Substitute.For<JwtSignatureVerifier>(new object[] { null });
+
+            this.plugin = new JwtHandlerPlugin(
+                this.register,
+                this.validator,
+                this.verifier);
+        }
+
+        public sealed class Order : JwtHandlerPluginTests
+        {
+            [Fact]
+            public void ShouldReturnAPositiveValue()
+            {
+                this.plugin.Order
+                    .Should().BePositive();
+            }
+        }
+
+        public sealed class ProcessAsync : JwtHandlerPluginTests
+        {
+            private static readonly MethodInfo AnonymousMethod =
+                typeof(ProcessAsync).GetMethod(nameof(Anonymous), BindingFlags.NonPublic | BindingFlags.Static);
+
+            private static readonly MethodInfo AuthenticatedMethod =
+                typeof(ProcessAsync).GetMethod(nameof(Authenticated), BindingFlags.NonPublic | BindingFlags.Static);
+
+            [Fact]
+            public async Task ShouldAllowAnonymousMethods()
+            {
+                IRequestData request = Substitute.For<IRequestData>();
+                request.Handler.Returns(AnonymousMethod);
+
+                IResponseData response = await this.plugin.ProcessAsync(request);
+
+                response.Should().BeNull();
+            }
+
+            [Fact]
+            public async Task ShouldNotAllowDifferentAuthorizationTypes()
+            {
+                // Example taken from RFC 7617
+                IRequestData request = CreateRequest("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==");
+
+                IResponseData response = await this.plugin.ProcessAsync(request);
+
+                response.StatusCode.Should().Be(Unauthorized);
+            }
+
+            [Fact]
+            public async Task ShouldNotAllowInvalidPayloads()
+            {
+                IRequestData request = CreateRequest("Bearer 123");
+                this.verifier.IsSignatureValid(null, out byte[] _)
+                    .ReturnsForAnyArgs(true);
+                this.validator.GetValidClaimsPrincipal(null)
+                    .ReturnsForAnyArgs((ClaimsPrincipal)null);
+
+                IResponseData response = await this.plugin.ProcessAsync(request);
+
+                response.StatusCode.Should().Be(Unauthorized);
+            }
+
+            [Fact]
+            public async Task ShouldNotAllowInvalidSignatures()
+            {
+                IRequestData request = CreateRequest("Bearer 123");
+                byte[] payload = Arg.Any<byte[]>();
+                this.verifier.IsSignatureValid("123", out payload)
+                    .Returns(false);
+
+                IResponseData response = await this.plugin.ProcessAsync(request);
+
+                // Make sure we got the right response and that we didn't try
+                // to get the claims from an invalid JWT
+                response.StatusCode.Should().Be(Unauthorized);
+                this.validator.DidNotReceiveWithAnyArgs()
+                    .GetValidClaimsPrincipal(null);
+            }
+
+            [Fact]
+            public async Task ShouldNotAllowMissingAuthorizationHeader()
+            {
+                IRequestData request = Substitute.For<IRequestData>();
+                request.Handler.Returns(AuthenticatedMethod);
+
+                IResponseData response = await this.plugin.ProcessAsync(request);
+
+                response.StatusCode.Should().Be(Unauthorized);
+            }
+
+            [Fact]
+            public async Task ShouldRegisterTheClaimsPrincipal()
+            {
+                IRequestData request = CreateRequest("Bearer 123");
+                byte[] payload = new byte[0];
+                var principal = new ClaimsPrincipal();
+
+                byte[] any = Arg.Any<byte[]>();
+                this.verifier.IsSignatureValid("123", out any)
+                    .Returns(ci =>
+                    {
+                        ci[1] = payload;
+                        return true;
+                    });
+
+                this.validator.GetValidClaimsPrincipal(payload)
+                    .Returns(principal);
+
+                IResponseData response = await this.plugin.ProcessAsync(request);
+
+                response.Should().BeNull();
+                this.register.Received().UseInstance(typeof(IPrincipal), principal);
+            }
+
+            [AllowAnonymous]
+            private static void Anonymous()
+            {
+            }
+
+            private static void Authenticated()
+            {
+            }
+
+            private static IRequestData CreateRequest(string authorization)
+            {
+                IRequestData request = Substitute.For<IRequestData>();
+                request.Handler.Returns(AuthenticatedMethod);
+                request.Headers.Returns(new Dictionary<string, string>
+                {
+                    { "Authorization", authorization }
+                });
+
+                return request;
+            }
+        }
+    }
+}
