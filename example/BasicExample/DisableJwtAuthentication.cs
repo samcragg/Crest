f28f5b6f27df2a1e4f3f3d6ff60dc85ecad4d455
Samuel Cragg <sam.cragg@outlook.com>
2018-04-09 06:18:34 +0100

    Update the example to skip authentication

---
+++ example/BasicExample/DisableJwtAuthentication.cs
@@ -0,0 +1,30 @@
+﻿namespace BasicExample
+{
+    using System;
+    using System.Collections.Generic;
+    using Crest.Abstractions;
+
+    /// <summary>
+    /// Shows an example of how to disable JWT authentication.
+    /// </summary>
+    public sealed class DisableJwtAuthentication : IJwtSettings
+    {
+        /// <inheritdoc />
+        public ISet<string> Audiences { get; }
+
+        /// <inheritdoc />
+        public string AuthenticationType { get; }
+
+        /// <inheritdoc />
+        public TimeSpan ClockSkew { get; }
+
+        /// <inheritdoc />
+        public ISet<string> Issuers { get; }
+
+        /// <inheritdoc />
+        public IReadOnlyDictionary<string, string> JwtClaimMappings { get; }
+
+        /// <inheritdoc />
+        public bool SkipAuthentication => true;
+    }
+}
