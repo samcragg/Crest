fe56ebf977516c9a0067f635f5b7369149b47d72
Samuel Cragg <sam.cragg@outlook.com>
2018-04-29 07:04:49 +0100

    Allow the appsettings.json to be injected into configuration classes

---
+++ src/Crest.Host/Engine/JsonConfigurationProvider.cs
@@ -0,0 +1,219 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Crest.Abstractions;
+    using Crest.Host.IO;
+    using Crest.Host.Logging;
+
+    /// <summary>
+    /// Injects the application JSON settings for the properties in a
+    /// configuration class.
+    /// </summary>
+    internal sealed class JsonConfigurationProvider : IConfigurationProvider, IDisposable
+    {
+        private const string GlobalSettingsFile = "appsettings.json";
+        private static readonly ILog Logger = LogProvider.For<JsonConfigurationProvider>();
+
+        private readonly string environmentSettingsFile;
+        private readonly JsonClassGenerator generator;
+
+        private readonly Dictionary<Type, TypeInitializer> initializers =
+            new Dictionary<Type, TypeInitializer>();
+
+        private readonly FileReader reader;
+        private readonly FileWriteWatcher watcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonConfigurationProvider"/> class.
+        /// </summary>
+        /// <param name="reader">Used to read the contents of files.</param>
+        /// <param name="watcher">Used to listen to file system events.</param>
+        /// <param name="generator">Used to generate the object initializer.</param>
+        public JsonConfigurationProvider(
+            FileReader reader,
+            FileWriteWatcher watcher,
+            JsonClassGenerator generator)
+        {
+            this.environmentSettingsFile = GetEnvironmentSettingsFileName();
+            this.generator = generator;
+            this.reader = reader;
+            this.watcher = watcher;
+
+            this.watcher.WatchFile(this.environmentSettingsFile, this.UpdateEnvironmentSettingsAsync);
+            this.watcher.WatchFile(GlobalSettingsFile, this.UpdateGlobalSettingsAsync);
+        }
+
+        /// <inheritdoc />
+        public int Order => 200;
+
+        /// <summary>
+        /// Releases the resources used by this instance.
+        /// </summary>
+        public void Dispose()
+        {
+            this.watcher.Dispose();
+        }
+
+        /// <inheritdoc />
+        public async Task Initialize(IEnumerable<Type> knownTypes)
+        {
+            foreach (Type type in knownTypes)
+            {
+                this.initializers.Add(type, new TypeInitializer(type));
+            }
+
+            await Task.WhenAll(this.UpdateEnvironmentSettingsAsync(), this.UpdateGlobalSettingsAsync())
+                .ConfigureAwait(false);
+
+            // Now that we've loaded the files, keep an eye on them
+            this.watcher.StartMonitoring();
+        }
+
+        /// <inheritdoc />
+        public void Inject(object instance)
+        {
+            Type type = instance.GetType();
+            if (this.initializers.TryGetValue(type, out TypeInitializer initializer))
+            {
+                // Apply the global settings first so that any environment
+                // specific setting overwrite them
+                initializer.GlobalSettings(instance);
+                initializer.EnvironmentSettings(instance);
+            }
+        }
+
+        private static string GetEnvironmentSettingsFileName()
+        {
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (string.IsNullOrEmpty(environment))
+            {
+                // The environment defaults to production if it's not specified:
+                // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/environments
+                Logger.WarnFormat("No environment detected (ASPNETCORE_ENVIRONMENT), assuming production");
+                return "appsettings.Production.json";
+            }
+            else
+            {
+                Logger.InfoFormat("Environment detected as '{environment}'", environment);
+                return "appsettings." + environment + ".json";
+            }
+        }
+
+        private TypeInitializer FindInitializer(string typeName)
+        {
+            IEnumerable<Type> matchingTypes =
+                this.initializers.Keys
+                    .Where(t => t.FullName.EndsWith(typeName, StringComparison.OrdinalIgnoreCase));
+
+            // We want to try to find only a single match and warn when it's
+            // ambiguous so the user can fully qualify the name
+            using (IEnumerator<Type> e = matchingTypes.GetEnumerator())
+            {
+                if (!e.MoveNext())
+                {
+                    Logger.WarnFormat(
+                        "No configuration class found for configuration setting '{key}'",
+                        typeName);
+                    return null;
+                }
+
+                Type type = e.Current;
+                if (e.MoveNext())
+                {
+                    Logger.WarnFormat(
+                        "Configuration setting '{key}' matches multiple types - try fully qualifying the type with its namespace",
+                        typeName);
+                    return null;
+                }
+
+                return this.initializers[type];
+            }
+        }
+
+        private Task UpdateEnvironmentSettingsAsync()
+        {
+            return this.UpdateTypeInitializerAsync(
+                this.environmentSettingsFile,
+                (ti, init) => ti.EnvironmentSettings = init);
+        }
+
+        private Task UpdateGlobalSettingsAsync()
+        {
+            return this.UpdateTypeInitializerAsync(
+                GlobalSettingsFile,
+                (ti, init) => ti.GlobalSettings = init);
+        }
+
+        private void UpdateTypeInitializer(
+            string typeName,
+            string json,
+            Action<TypeInitializer, Action<object>> updateInitializer)
+        {
+            TypeInitializer initializer = this.FindInitializer(typeName);
+            if (initializer != null)
+            {
+                Action<object> action = this.generator.CreatePopulateMethod(
+                    initializer.Type,
+                    json);
+
+                updateInitializer(initializer, action);
+            }
+        }
+
+        private async Task UpdateTypeInitializerAsync(
+            string filename,
+            Action<TypeInitializer, Action<object>> updateInitializer)
+        {
+            try
+            {
+                byte[] json = await this.reader.ReadAllBytesAsync(filename).ConfigureAwait(false);
+                var parser = new JsonObjectParser(json);
+                foreach (KeyValuePair<string, string> pair in parser.GetPairs())
+                {
+                    this.UpdateTypeInitializer(pair.Key, pair.Value, updateInitializer);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Logger.ErrorException("'{filename}' contains invalid JSON", ex, filename);
+            }
+            catch (IOException ex)
+            {
+                Logger.ErrorException("Unable to read '{filename}'", ex, filename);
+            }
+        }
+
+        private class TypeInitializer
+        {
+            public TypeInitializer(Type type)
+            {
+                this.EnvironmentSettings = DoNothing;
+                this.GlobalSettings = DoNothing;
+                this.Type = type;
+            }
+
+            public static Action<object> DoNothing { get; } = EmptyMethod;
+
+            public Action<object> EnvironmentSettings { get; set; }
+
+            public Action<object> GlobalSettings { get; set; }
+
+            public Type Type { get; }
+
+            private static void EmptyMethod(object instance)
+            {
+                // This method is intentionally empty to allow the actions
+                // to always be invoked
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Engine/JsonConfigurationProviderTests.cs
@@ -0,0 +1,203 @@
+﻿namespace Host.UnitTests.Engine
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Crest.Host.Engine;
+    using Crest.Host.IO;
+    using Crest.Host.Logging;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    // As we're playing with an environment variable, we can't have the nested
+    // tests running in parallel
+    [Collection(nameof(JsonConfigurationProviderTests))]
+    public class JsonConfigurationProviderTests
+    {
+        private const string EnvironmentSettingsFile = "appsettings.UnitTests.json";
+        private const string GlobalSettingsFile = "appsettings.json";
+        private readonly JsonClassGenerator generator;
+        private readonly FileReader reader;
+        private readonly FileWriteWatcher watcher;
+        private JsonConfigurationProvider provider;
+
+        private JsonConfigurationProviderTests()
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "UnitTests");
+            this.generator = Substitute.For<JsonClassGenerator>();
+            this.reader = Substitute.For<FileReader>();
+            this.watcher = Substitute.For<FileWriteWatcher>();
+            this.provider = new JsonConfigurationProvider(this.reader, this.watcher, this.generator);
+        }
+
+        public sealed class Dispose : JsonConfigurationProviderTests
+        {
+            [Fact]
+            public void ShouldDisposeTheFileWriteWatcher()
+            {
+                this.provider.Dispose();
+
+                this.watcher.Received().Dispose();
+            }
+        }
+
+        public sealed class Initialize : JsonConfigurationProviderTests
+        {
+            [Fact]
+            public async Task ShouldDefaultToProductionIfNoEnvironmentSet()
+            {
+                Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", string.Empty);
+                this.provider = new JsonConfigurationProvider(this.reader, this.watcher, this.generator);
+
+                await this.provider.Initialize(new Type[0]);
+
+                await this.reader.Received().ReadAllBytesAsync("appsettings.Production.json");
+            }
+
+            [Fact]
+            public async Task ShouldLoadTheEnvironmentSettings()
+            {
+                await this.provider.Initialize(new Type[0]);
+
+                await this.reader.Received().ReadAllBytesAsync(EnvironmentSettingsFile);
+            }
+
+            [Fact]
+            public async Task ShouldLoadTheGlobalSettings()
+            {
+                await this.provider.Initialize(new Type[0]);
+
+                await this.reader.Received().ReadAllBytesAsync(GlobalSettingsFile);
+            }
+
+            [Fact]
+            public async Task ShouldLogAmbiguousJsonKeys()
+            {
+                using (FakeLogger.LogInfo logger = FakeLogger.MonitorLogging())
+                {
+                    this.reader.ReadAllBytesAsync(GlobalSettingsFile)
+                        .Returns(Encoding.ASCII.GetBytes(@"{""myConfig"":{}}"));
+
+                    await this.provider.Initialize(new[]
+                    {
+                        typeof(DuplicateClass1.MyConfig),
+                        typeof(DuplicateClass2.MyConfig)
+                    });
+
+                    logger.LogLevel.Should().Be(LogLevel.Warn);
+                    logger.Message.Should().MatchEquivalentOf("*multiple*");
+                }
+            }
+
+            [Fact]
+            public async Task ShouldLogIOExceptions()
+            {
+                using (FakeLogger.LogInfo logger = FakeLogger.MonitorLogging())
+                {
+                    this.reader.ReadAllBytesAsync(null)
+                        .ReturnsForAnyArgs<byte[]>(_ => throw new IOException());
+
+                    await this.provider.Initialize(new Type[0]);
+
+                    logger.LogLevel.Should().Be(LogLevel.Error);
+                    logger.Message.Should().MatchEquivalentOf("*unable to read*");
+                }
+            }
+
+            [Fact]
+            public async Task ShouldLogJsonExceptions()
+            {
+                using (FakeLogger.LogInfo logger = FakeLogger.MonitorLogging())
+                {
+                    this.generator.CreatePopulateMethod(null, null)
+                        .ReturnsForAnyArgs(_ => throw new FormatException());
+
+                    await this.provider.Initialize(new[] { typeof(string) });
+
+                    logger.LogLevel.Should().Be(LogLevel.Error);
+                    logger.Message.Should().MatchEquivalentOf("*invalid json*");
+                }
+            }
+
+            [Fact]
+            public async Task ShouldLogUnknownJsonKeys()
+            {
+                using (FakeLogger.LogInfo logger = FakeLogger.MonitorLogging())
+                {
+                    this.reader.ReadAllBytesAsync(GlobalSettingsFile)
+                        .Returns(Encoding.ASCII.GetBytes(@"{""myConfig"":{}}"));
+
+                    await this.provider.Initialize(new Type[0]);
+
+                    logger.LogLevel.Should().Be(LogLevel.Warn);
+                    logger.Message.Should().MatchEquivalentOf("*found*");
+                }
+            }
+
+            private class DuplicateClass1
+            {
+                public class MyConfig
+                {
+                }
+            }
+
+            private class DuplicateClass2
+            {
+                public class MyConfig
+                {
+                }
+            }
+        }
+
+        public sealed class Inject : JsonConfigurationProviderTests
+        {
+            [Fact]
+            public void ShouldIgnoreTypesItCantHandle()
+            {
+                Action action = () => this.provider.Inject(new object());
+
+                action.Should().NotThrow();
+            }
+
+            [Fact]
+            public async Task ShouldOverwriteGlobalSettingsWithEnvironmentSettings()
+            {
+                byte[] environemnt = Encoding.ASCII.GetBytes(@"{""myConfig"":""environment""}");
+                this.reader.ReadAllBytesAsync(EnvironmentSettingsFile).Returns(environemnt);
+
+                byte[] global = Encoding.ASCII.GetBytes(@"{""myConfig"":""global""}");
+                this.reader.ReadAllBytesAsync(GlobalSettingsFile).Returns(global);
+
+                this.generator.CreatePopulateMethod(typeof(MyConfig), Arg.Any<string>())
+                    .Returns(ci =>
+                    {
+                        return x => ((MyConfig)x).Value = ci.Arg<string>();
+                    });
+
+                await this.provider.Initialize(new[] { typeof(MyConfig) });
+                var instance = new MyConfig();
+                this.provider.Inject(instance);
+
+                instance.Value.Should().Be("environment");
+            }
+
+            private class MyConfig
+            {
+                public string Value { get; set; }
+            }
+        }
+
+        public sealed class Order : JsonConfigurationProviderTests
+        {
+            [Fact]
+            public void ShouldReturnAValueGreaterThanTheDefaultValuePropervider()
+            {
+                var defaultProvider = new DefaultConfigurationProvider();
+
+                this.provider.Order.Should().BeGreaterThan(defaultProvider.Order);
+            }
+        }
+    }
+}
