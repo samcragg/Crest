9b96c93ebab8136acb61b9d0cce0442328f96937
Samuel Cragg <sam.cragg@outlook.com>
2018-04-29 06:41:15 +0100

    Allow monitoring of file changes

---
+++ src/Crest.Host/IO/FileWriteWatcher.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Listens to the file system change notifications for specific files when
+    /// they have been written to.
+    /// </summary>
+    internal class FileWriteWatcher : IDisposable
+    {
+        private readonly Dictionary<string, Func<Task>> files =
+            new Dictionary<string, Func<Task>>(StringComparer.OrdinalIgnoreCase);
+
+        private readonly FileSystemWatcher watcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileWriteWatcher"/> class.
+        /// </summary>
+        public FileWriteWatcher()
+        {
+            this.watcher = new FileSystemWatcher
+            {
+                NotifyFilter = NotifyFilters.LastWrite,
+                IncludeSubdirectories = false,
+                Path = AppContext.BaseDirectory
+            };
+
+            this.watcher.Changed += this.OnFileSystemChanged;
+            this.watcher.Created += this.OnFileSystemChanged;
+            this.watcher.Deleted += this.OnFileSystemChanged;
+        }
+
+        /// <summary>
+        /// Releases the resources used by this instance.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            this.watcher.Dispose();
+        }
+
+        /// <summary>
+        /// Starts monitoring of file system events.
+        /// </summary>
+        public virtual void StartMonitoring()
+        {
+            this.watcher.EnableRaisingEvents = true;
+        }
+
+        /// <summary>
+        /// Associates the specified callback with changes to the specified
+        /// filename.
+        /// </summary>
+        /// <param name="filename">
+        /// The name of the file, relative to the applications base directory.
+        /// </param>
+        /// <param name="callback">
+        /// The action to invoke when a change is raised.
+        /// </param>
+        public virtual void WatchFile(string filename, Func<Task> callback)
+        {
+            this.files.Add(filename, callback);
+        }
+
+        private void OnFileSystemChanged(object sender, FileSystemEventArgs e)
+        {
+            if (this.files.TryGetValue(e.Name, out Func<Task> callback))
+            {
+                Task.Run(callback);
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/IO/FileWriteWatcherTests.cs
@@ -0,0 +1,69 @@
+﻿namespace Host.UnitTests.IO
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Crest.Host.IO;
+    using FluentAssertions;
+    using Xunit;
+
+    public class FileWriteWatcherTests : IDisposable
+    {
+        private readonly FileWriteWatcher watcher;
+
+        public FileWriteWatcherTests()
+        {
+            this.watcher = new FileWriteWatcher();
+        }
+
+        public void Dispose()
+        {
+            this.watcher.Dispose();
+        }
+
+        public class StartMonitoring : FileWriteWatcherTests
+        {
+            [Fact]
+            public async Task ShouldListenForFileChanges()
+            {
+                const string Filename = nameof(FileWriteWatcherTests);
+
+                try
+                {
+                    var semaphore = new SemaphoreSlim(0);
+                    Func<Task> callback = () =>
+                    {
+                        semaphore.Release();
+                        return Task.CompletedTask;
+                    };
+
+                    this.watcher.WatchFile(Filename, callback);
+                    this.watcher.StartMonitoring();
+
+                    File.WriteAllText(Filename, "{}");
+                    bool success = await semaphore.WaitAsync(TimeSpan.FromSeconds(1));
+
+                    success.Should().BeTrue();
+                }
+                finally
+                {
+                    FileReaderTests.TryDeleteFile(Filename);
+                }
+            }
+        }
+
+        public class WatchFile : FileWriteWatcherTests
+        {
+            [Fact]
+            public void ShouldThrowIfTheFileIsRegisteredTwice()
+            {
+                this.watcher.WatchFile("example.txt", null);
+
+                Action action = () => this.watcher.WatchFile("example.txt", null);
+
+                action.Should().Throw<ArgumentException>();
+            }
+        }
+    }
+}
