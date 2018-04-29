01207cab6798ceca4ceaa276810a7e3712e9fd0e
Samuel Cragg <sam.cragg@outlook.com>
2018-04-29 06:37:19 +0100

    Allow the asynchronous reading of files

---
+++ src/Crest.Host/IO/FileReader.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.IO
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Allows the reading of files.
+    /// </summary>
+    internal class FileReader
+    {
+        /// <summary>
+        /// Reads the entire contents of a file into a byte array.
+        /// </summary>
+        /// <param name="path">The file to open for reading.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The value of the
+        /// <c>TResult</c> parameter is a byte array containing the contents of
+        /// the file.
+        /// </returns>
+        public virtual async Task<byte[]> ReadAllBytesAsync(string path)
+        {
+            using (Stream file = this.OpenFile(path))
+            {
+                int index = 0;
+                int count = (int)file.Length;
+                byte[] bytes = new byte[count];
+                while (count > 0)
+                {
+                    int read = await file.ReadAsync(bytes, index, count).ConfigureAwait(false);
+                    if (read == 0)
+                    {
+                        throw new EndOfStreamException();
+                    }
+
+                    index += read;
+                    count -= read;
+                }
+
+                return bytes;
+            }
+        }
+
+        /// <summary>
+        /// Opens a file in asynchronous mode.
+        /// </summary>
+        /// <param name="path">A relative or absolute path for the file.</param>
+        /// <returns>The file stream.</returns>
+        protected virtual Stream OpenFile(string path)
+        {
+            return new FileStream(
+                path,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read,
+                bufferSize: 4096,
+                useAsync: true);
+        }
+    }
+}
---
+++ test/Host.UnitTests/IO/FileReaderTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Host.UnitTests.IO
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Crest.Host.IO;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    public class FileReaderTests
+    {
+        private readonly FakeFileReader reader = new FakeFileReader();
+
+        internal static void TryDeleteFile(string path)
+        {
+            try
+            {
+                File.Delete(path);
+            }
+            catch
+            {
+            }
+        }
+
+        public sealed class ReadAllBytesAsync : FileReaderTests
+        {
+            [Fact]
+            public async Task ShouldReadTheFileAtTheSpecifiedLocation()
+            {
+                string filename = Path.GetTempFileName();
+                try
+                {
+                    File.WriteAllText(filename, "Test");
+
+                    byte[] contents = await this.reader.ReadAllBytesAsync(filename);
+
+                    Encoding.ASCII.GetString(contents)
+                        .Should().Be("Test");
+                }
+                finally
+                {
+                    TryDeleteFile(filename);
+                }
+            }
+
+            [Fact]
+            public void ShouldThrowIfTheEndOfStreamIsReachedBeforeAllTheContentIsRead()
+            {
+                this.reader.SourceStream = Substitute.For<Stream>();
+                this.reader.SourceStream.Length.Returns(100);
+
+                Func<Task> action = () => this.reader.ReadAllBytesAsync("");
+
+                action.Should().Throw<EndOfStreamException>();
+            }
+        }
+
+        private class FakeFileReader : FileReader
+        {
+            internal Stream SourceStream { get; set; }
+
+            protected override Stream OpenFile(string path)
+            {
+                if (this.SourceStream == null)
+                {
+                    return base.OpenFile(path);
+                }
+                else
+                {
+                    return this.SourceStream;
+                }
+            }
+        }
+    }
+}
