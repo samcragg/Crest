929975a5bd8b3e9cc3680e33ba7da2a5d3c1e727
Samuel Cragg <sam.cragg@outlook.com>
2018-07-28 07:01:37 +0100

    Add time unit

---
+++ src/Crest.Host/Diagnostics/TimeUnit.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Diagnostics
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// Represents units of time.
+    /// </summary>
+    internal sealed class TimeUnit : IUnit
+    {
+        /// <summary>
+        /// Gets an instance of this class.
+        /// </summary>
+        internal static TimeUnit Instance { get; } = new TimeUnit();
+
+        /// <inheritdoc />
+        public string Format(long value)
+        {
+            if (value <= 0)
+            {
+                return "0";
+            }
+            else if (value < 1000)
+            {
+                return value.ToString(NumberFormatInfo.InvariantInfo) + "µs";
+            }
+            else if (value < (1000 * 1000))
+            {
+                return (value / 1_000.0).ToString("f1", NumberFormatInfo.InvariantInfo) + "ms";
+            }
+            else
+            {
+                return (value / 1_000_000.0).ToString("f1", NumberFormatInfo.InvariantInfo) + "s";
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Diagnostics/TimeUnitTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Host.UnitTests.Diagnostics
+{
+    using Crest.Host.Diagnostics;
+    using FluentAssertions;
+    using Host.UnitTests.TestHelpers;
+    using Xunit;
+
+    public class TimeUnitTests
+    {
+        private readonly TimeUnit time = new TimeUnit();
+
+        public sealed class Format : TimeUnitTests
+        {
+            [Fact]
+            public void ShouldNotOutputNegativeTimes()
+            {
+                string result = this.time.Format(-1);
+
+                result.Should().Be("0");
+            }
+
+            [Fact]
+            public void ShouldOutputMicroSeconds()
+            {
+                string result = this.time.Format(20);
+
+                result.Should().Be("20µs");
+            }
+
+            [Fact]
+            public void ShouldOutputTheTimingsInMilliSeconds()
+            {
+                string result = this.time.Format(2_000);
+
+                result.Should().Be("2.0ms");
+            }
+
+            [Fact]
+            public void ShouldOutputTheTimingsInSeconds()
+            {
+                string result = this.time.Format(2_000_000);
+
+                result.Should().Be("2.0s");
+            }
+
+            [Fact]
+            [UseCulture("ES")]
+            public void ShouldUseTheInvariantCulture()
+            {
+                string result = this.time.Format(1_200);
+
+                result.Should().Be("1.2ms");
+            }
+        }
+    }
+}
