3d704fbce2726125a8f15c8ed3f6e84f1407da99
Samuel Cragg <sam.cragg@outlook.com>
2018-08-04 06:38:07 +0100

    Add integration tests for health and metric pages

--- test/IntegrationTests/BuiltInPagesTests.cs
+++ test/IntegrationTests/BuiltInPagesTests.cs
@@ -5,6 +5,7 @@
     using System.Threading.Tasks;
     using FluentAssertions;
     using HtmlAgilityPack;
+    using Newtonsoft.Json.Linq;
     using Xunit;
 
     [Trait("Category", "Integration")]
@@ -33,25 +34,64 @@
         [Fact]
         public async Task EnsureDocsReturnsTheHtmlViewer()
         {
-            using (HttpClient client = this.fixture.CreateClient())
-            {
-                var htmlDoc = new HtmlDocument();
-                htmlDoc.LoadHtml(await client.GetStringAsync("/docs/index.html"));
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(await this.GetResultAsync("/docs/index.html"));
 
-                htmlDoc.DocumentNode.SelectSingleNode("//body")
-                    .Should().NotBeNull();
-            }
+            htmlDoc.DocumentNode.SelectSingleNode("//body")
+                .Should().NotBeNull();
         }
 
         [Fact]
         public async Task EnsureDocsReturnTheOpenApiData()
         {
-            using (HttpClient client = this.fixture.CreateClient())
-            {
-                string result = await client.GetStringAsync("/docs/V1/OpenAPI.json");
+            string result = await this.GetResultAsync("/docs/V1/OpenAPI.json");
 
-                result.Should().Be("Documentation information");
+            result.Should().Be("Documentation information");
+        }
+
+        [Fact]
+        public async Task EnsureHealthPageIsPresent()
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(await this.GetResultAsync("/health"));
+
+            htmlDoc.DocumentNode.SelectSingleNode("//h1").InnerText
+                .Should().Be("Service Health");
+        }
+
+        [Fact]
+        public async Task EnsureMetricsJsonIsPresent()
+        {
+            string result = await this.GetResultAsync("/metrics.json");
+
+            var json = JToken.Parse(result);
+            json.Should().Contain(j => j.Path == "requestCount");
+        }
+
+        private async Task<string> GetResultAsync(string url)
+        {
+            // There's an issue with the TestServer where it can end the request
+            // if it takes too long. For now, brute force it by retrying...
+            for (int i = 0; i < 16; i++)
+            {
+                HttpClient client = this.fixture.CreateClient();
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.IsSuccessStatusCode.Should().BeTrue();
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException)
+                {
+                }
+                finally
+                {
+                    client.Dispose();
+                }
             }
+
+            throw new Xunit.Sdk.XunitException("Unable to read the response.");
         }
     }
 }
