fdf15d7b03a96f3a8048127c61b6fa31e370a742
Samuel Cragg <sam.cragg@outlook.com>
2018-09-23 08:45:13 +0100

    Add assembly information in the properties folder

---
+++ src/Crest.DataAccess/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Allow NSubstitute to mock our types
+[assembly: InternalsVisibleTo("DataAccess.UnitTests")]
