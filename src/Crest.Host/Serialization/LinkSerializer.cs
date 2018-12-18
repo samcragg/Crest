18d7bf7df0687e2ac949df83a341dd4e2cd753ce
Samuel Cragg <sam.cragg@outlook.com>
2018-12-18 08:00:58 +0000

    Allow the serialization of link collections

---
+++ src/Crest.Host/Serialization/LinkSerializer.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Crest.Host.Serialization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Crest.Core;
+    using Crest.Host.Serialization.Internal;
+
+    /// <summary>
+    /// Allows the serialization of <see cref="LinkCollection"/>.
+    /// </summary>
+    internal sealed class LinkSerializer
+    {
+        private readonly IClassWriter writer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkSerializer"/> class.
+        /// </summary>
+        /// <param name="writer">Used to write the object to.</param>
+        public LinkSerializer(IClassWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        /// <summary>
+        /// Serializes the specified instance.
+        /// </summary>
+        /// <param name="links">The object to serialize.</param>
+        public void Serialize(LinkCollection links)
+        {
+            this.writer.WriteBeginClass(nameof(LinkCollection));
+            foreach (IGrouping<string, Link> group in (ILookup<string, Link>)links)
+            {
+                this.writer.WriteBeginProperty(group.Key);
+                this.SerializeLinks((IReadOnlyCollection<Link>)group);
+                this.writer.WriteEndProperty();
+            }
+
+            this.writer.WriteEndClass();
+        }
+
+        private void SerializeLinks(IReadOnlyCollection<Link> links)
+        {
+            int count = links.Count;
+            if (count == 1)
+            {
+                this.SerializeLink(links.FirstOrDefault());
+            }
+            else
+            {
+                this.writer.WriteBeginArray(typeof(Link), count);
+                foreach (Link link in links)
+                {
+                    this.SerializeLink(link);
+
+                    // Are we expecting more?
+                    count--;
+                    if (count > 0)
+                    {
+                        this.writer.WriteElementSeparator();
+                    }
+                }
+
+                this.writer.WriteEndArray();
+            }
+        }
+
+        private void SerializeLink(Link link)
+        {
+            this.SerializeNonNullProperty(nameof(Link.HRef), link.HRef);
+            if (link.Templated)
+            {
+                this.writer.WriteBeginProperty(nameof(Link.Templated));
+                this.writer.Writer.WriteBoolean(true);
+                this.writer.WriteEndProperty();
+            }
+
+            this.SerializeNonNullProperty(nameof(Link.Type), link.Type);
+            this.SerializeNonNullProperty(nameof(Link.Deprecation), link.Deprecation);
+            this.SerializeNonNullProperty(nameof(Link.Name), link.Name);
+            this.SerializeNonNullProperty(nameof(Link.Profile), link.Profile);
+            this.SerializeNonNullProperty(nameof(Link.Title), link.Title);
+            this.SerializeNonNullProperty(nameof(Link.HRefLang), link.HRefLang);
+        }
+
+        private void SerializeNonNullProperty(string propertyName, string value)
+        {
+            if (value != null)
+            {
+                this.writer.WriteBeginProperty(propertyName);
+                this.writer.Writer.WriteString(value);
+                this.writer.WriteEndProperty();
+            }
+        }
+
+        private void SerializeNonNullProperty(string propertyName, Uri value)
+        {
+            if (value != null)
+            {
+                this.writer.WriteBeginProperty(propertyName);
+                this.writer.Writer.WriteUri(value);
+                this.writer.WriteEndProperty();
+            }
+        }
+    }
+}
---
+++ test/Host.UnitTests/Serialization/LinkSerializerTests.cs
@@ -0,0 +1,106 @@
+namespace Host.UnitTests.Serialization
+{
+    using System;
+    using Crest.Core;
+    using Crest.Host.Serialization;
+    using Crest.Host.Serialization.Internal;
+    using NSubstitute;
+    using Xunit;
+
+    public class LinkSerializerTests
+    {
+        private readonly LinkSerializer serializer;
+        private readonly IClassWriter writer;
+
+        private LinkSerializerTests()
+        {
+            this.writer = Substitute.For<IClassWriter>();
+            this.writer.Writer.Returns(Substitute.For<ValueWriter>());
+            this.serializer = new LinkSerializer(this.writer);
+        }
+
+        public sealed class Serialize : LinkSerializerTests
+        {
+            [Fact]
+            public void ShouldNotSerializeNameIfNull()
+            {
+                this.serializer.Serialize(new LinkCollection
+                {
+                    new Link(null, new Uri("http://www.example.com"), null, name: null, null, "relation", false, null, null)
+                });
+
+                this.writer.DidNotReceive().WriteBeginProperty(nameof(Link.Name));
+                this.writer.Writer.DidNotReceive().WriteString(null);
+            }
+
+            [Fact]
+            public void ShouldNotSerializeTemplatedIfFalse()
+            {
+                this.serializer.Serialize(new LinkCollection
+                {
+                    new Link(null, new Uri("http://www.example.com"), null, null, null, "relation", templated: false, null, null)
+                });
+
+                this.writer.DidNotReceive().WriteBeginProperty(nameof(Link.Templated));
+                this.writer.Writer.DidNotReceive().WriteBoolean(false);
+            }
+
+            [Fact]
+            public void ShouldSerializeMultipleLinks()
+            {
+                var route1 = new Uri("http://www.example.com/route1");
+                var route2 = new Uri("http://www.example.com/route2");
+                this.serializer.Serialize(new LinkCollection
+                {
+                    { "multiple", route1 },
+                    { "multiple", route2 },
+                });
+
+                this.writer.Received().WriteBeginProperty("multiple");
+                this.writer.Received().WriteBeginArray(typeof(Link), 2);
+                this.writer.Received(2).WriteBeginProperty(nameof(Link.HRef));
+                this.writer.Writer.Received().WriteUri(route1);
+                this.writer.Writer.Received().WriteUri(route2);
+            }
+
+            [Fact]
+            public void ShouldSerializeNameIfNotNull()
+            {
+                this.serializer.Serialize(new LinkCollection
+                {
+                    new Link(null, new Uri("http://www.example.com"), null, "name value", null, "relation", false, null, null)
+                });
+
+                this.writer.Received().WriteBeginProperty(nameof(Link.Name));
+                this.writer.Writer.Received().WriteString("name value");
+            }
+
+            [Fact]
+            public void ShouldSerializeSingleLinks()
+            {
+                var uri = new Uri("http://www.example.com");
+                this.serializer.Serialize(new LinkCollection
+                {
+                    { "single", uri }
+                });
+
+                this.writer.DidNotReceiveWithAnyArgs().WriteBeginArray(null, 0);
+                this.writer.Received().WriteBeginProperty("single");
+                this.writer.Received().WriteBeginProperty(nameof(Link.HRef));
+                this.writer.Writer.Received().WriteUri(uri);
+            }
+
+            [Fact]
+            public void ShouldSerializeTemplatedIfTrue()
+            {
+                this.serializer.Serialize(new LinkCollection
+                {
+                    new Link(null, new Uri("http://www.example.com"), null, null, null, "relation", templated: true, null, null)
+                });
+
+                this.writer.Received().WriteBeginProperty(nameof(Link.Templated));
+                this.writer.Writer.Received().WriteBoolean(true);
+            }
+        }
+    }
+}
